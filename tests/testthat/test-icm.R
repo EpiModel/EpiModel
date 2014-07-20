b1f4be49a0356885d956b9e7c30e716a6d7be610
Samuel Jenness <smjenness@gmail.com>
2014-07-19 19:49:22 -0700

    Added icm test cases to test-icm.R (closes #33)

--- tests/testthat/test-icm.R
+++ tests/testthat/test-icm.R
@@ -1,5 +1,119 @@
 context("icm")
 
+# SI Models ---------------------------------------------------------------
+
+test_that("SI, 1G, CL: 1 sim", {
+  param <- param.icm(trans.rate = 0.2, act.rate = 0.25)
+  init <- init.icm(s.num = 500, i.num = 1)
+  control <- control.icm(type = "SI", nsteps = 5, nsims = 1, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+test_that("SI, 1G, CL: 2 sims", {
+  param <- param.icm(trans.rate = 0.2, act.rate = 0.25)
+  init <- init.icm(s.num = 500, i.num = 1)
+  control <- control.icm(type = "SI", nsteps = 5, nsims = 2, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+test_that("SI, 1G, OP: 1 sim", {
+  param <- param.icm(trans.rate = 0.2,
+                     act.rate = 0.25, b.rate = 1/100,
+                     ds.rate = 1/100, di.rate = 1/90)
+  init <- init.icm(s.num = 500, i.num = 1)
+  control <- control.icm(type = "SI", nsteps = 5, nsims = 1, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+test_that("SI, 2G, OP: 2 sims", {
+  param <- param.icm(trans.rate = 0.2,
+                     act.rate = 0.25, trans.rate.g2 = 0.1, balance = "g1",
+                     b.rate = 1/100, b.rate.g2 = NA, ds.rate = 1/100,
+                     ds.rate.g2 = 1/100, di.rate = 1/90, di.rate.g2 = 1/90)
+  init <- init.icm(s.num = 500, i.num = 1, s.num.g2 = 500, i.num.g2 = 0)
+  control <- control.icm(type = "SI", nsteps = 5, nsims = 2, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+
+# SIR Models --------------------------------------------------------------
+
+test_that("SIR, 1G, CL: 1 sim", {
+  param <- param.icm(trans.rate = 0.2,
+                     act.rate = 0.25, rec.rate = 1/50,)
+  init <- init.icm(s.num = 500, i.num = 1, r.num = 0)
+  control <- control.icm(type = "SIR", nsteps = 5, nsims = 1, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+test_that("SIR, 1G, OP: 1 sim", {
+  param <- param.icm(trans.rate = 0.2,
+                     act.rate = 2, rec.rate = 1/50, b.rate = 1/100,
+                     ds.rate = 1/100, di.rate = 1/90, dr.rate = 1/100)
+  init <- init.icm(s.num = 500, i.num = 1, r.num = 0)
+  control <- control.icm(type = "SIR", nsteps = 5, nsims = 1, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+test_that("SIR, 2G, OP: 2 sims", {
+  param <- param.icm(trans.rate = 0.2, trans.rate.g2 = 0.1,
+                     act.rate = 1, balance = "g1",
+                     rec.rate = 1/50, rec.rate.g2 = 1/50,
+                     b.rate = 1/100, b.rate.g2 = NA,
+                     ds.rate = 1/100, ds.rate.g2 = 1/100,
+                     di.rate = 1/90, di.rate.g2 = 1/90,
+                     dr.rate = 1/100, dr.rate.g2 = 1/100)
+  init <- init.icm(s.num = 500, i.num = 1, r.num = 0,
+                   s.num.g2 = 500, i.num.g2 = 1, r.num.g2 = 0)
+  control <- control.icm(type = "SIR", nsteps = 5, nsims = 2, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+
+
+# SIS Models --------------------------------------------------------------
+
+test_that("SIS, 1G, CL: 1 sim", {
+  param <- param.icm(trans.rate = 0.2,
+                     act.rate = 0.25, rec.rate = 1/50)
+  init <- init.icm(s.num = 500, i.num = 1)
+  control <- control.icm(type = "SIS", nsteps = 5, nsims = 1, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+test_that("SIS, 2G, CL: 2 sims", {
+  param <- param.icm(trans.rate = 0.2,
+                     act.rate = 0.25, trans.rate.g2 = 0.1, balance = "g1",
+                     rec.rate = 1/100, rec.rate.g2 = 1/100)
+  init <- init.icm(s.num = 500, i.num = 1,
+                   s.num.g2 = 500, i.num.g2 = 0)
+  control <- control.icm(type = "SIS", nsteps = 5, nsims = 2, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+test_that("SIS, 1G, OP: 1 sim", {
+  param <- param.icm(trans.rate = 0.2,
+                     act.rate = 0.5, rec.rate = 1/50,
+                     b.rate = 1/100, ds.rate = 1/100, di.rate = 1/90)
+  init <- init.icm(s.num = 500, i.num = 1)
+  control <- control.icm(type = "SIS", nsteps = 10, nsims = 1, verbose = FALSE)
+  x <- icm(param, init, control)
+  expect_is(x, "icm")
+})
+
+
+
+# Other -------------------------------------------------------------------
+
 test_that("Act rate balance works when specified to g2", {
   param <- param.icm(trans.rate = 0.2,
                      trans.rate.g2 = 0.1,
@@ -18,7 +132,7 @@ test_that("Act rate balance works when specified to g2", {
                    s.num.g2 = 100,
                    i.num.g2 = 1)
   control <- control.icm(type = "SIS",
-                         nsteps = 25,
+                         nsteps = 10,
                          nsims = 1,
                          verbose = FALSE)
   x <- icm(param, init, control)
