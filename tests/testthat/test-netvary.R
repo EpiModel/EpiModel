79890ac4d93d1d8e2e9b4d4e403c83961455eb62
Samuel Jenness <smjenness@gmail.com>
2014-08-19 16:53:17 +0200

    Update tests to remove trans.rate refs

--- tests/testthat/test-netvary.R
+++ tests/testthat/test-netvary.R
@@ -1,8 +1,8 @@
-## Testing time-varying trans.rate and act.rate in network models
+## Testing time-varying inf.prob and act.rate in network models
 
 context("Time Varying Network Parameters")
 
-test_that("time varying trans.rate and act.rate for one-mode", {
+test_that("time varying inf.prob and act.rate for one-mode", {
   nw <- network.initialize(n = 100, directed = FALSE)
   formation <- ~ edges
   target.stats <- 50
@@ -20,13 +20,13 @@ test_that("time varying trans.rate and act.rate for one-mode", {
 
   probs <- c(0.25, 0.02)
   durs <- c(10, 1)
-  trans.rates <- rep(probs, durs)
+  inf.probs <- rep(probs, durs)
 
   acts <- c(0.5, 2)
   act.rates <- rep(acts, durs)
 
   # Parameters, initial conditions, and controls for model
-  param <- param.net(trans.rate = trans.rates, act.rate = act.rates)
+  param <- param.net(inf.prob = inf.probs, act.rate = act.rates)
   init <- init.net(i.num = 50)
   control <- control.net(type = "SI", nsteps = 10, nsims = 1,
                          save.transmat = TRUE, verbose = FALSE)
@@ -35,7 +35,7 @@ test_that("time varying trans.rate and act.rate for one-mode", {
 })
 
 
-test_that("time varying trans.rate and act.rate for bipartite", {
+test_that("time varying inf.prob and act.rate for bipartite", {
   nw <- network.initialize(n = 100, bipartite = 50, directed = FALSE)
   formation <- ~ edges
   target.stats <- 50
@@ -53,14 +53,14 @@ test_that("time varying trans.rate and act.rate for bipartite", {
 
   probs <- c(0.25, 0.01)
   durs <- c(10, 1)
-  trans.rates <- rep(probs, durs)
-  trans.rates.m2 <- trans.rates*2
+  inf.probs <- rep(probs, durs)
+  inf.probs.m2 <- inf.probs*2
 
   acts <- c(1, 2)
   act.rates <- rep(acts, durs)
 
-  param <- param.net(trans.rate = trans.rates,
-                     trans.rate.m2 = trans.rates.m2,
+  param <- param.net(inf.prob = inf.probs,
+                     inf.prob.m2 = inf.probs.m2,
                      act.rate = act.rates)
   init <- init.net(i.num = 10, i.num.m2 = 10)
   control <- control.net(type = "SI", nsteps = 10, nsims = 1,
