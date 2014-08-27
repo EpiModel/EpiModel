3132c45177a2bc837f4671225a334c9a081cf040
Samuel Jenness <smjenness@gmail.com>
2014-08-27 12:44:24 +0200

    Fix bug in netdx for offsets (closes #84)
    Bug was due to mismatch between target statistics vector and formation formula vector, also added corresponding tests.

--- R/netdx.R
+++ R/netdx.R
@@ -194,9 +194,17 @@ netdx <- function(x,
                             names = names(stats.means),
                             stats.means, stats.sd)
 
+  # Which formation terms are offsets?
+  is.offset.term <- grep(pattern = "offset",
+                         strsplit(as.character(formation), "[+]")[[2]])
 
-  ## Get stats from for target statistics
-  ts.out <- data.frame(names = attributes(diag.sim.ts)$dimnames[[2]],
+
+  ## Get stats from for target statistics, removing offsets
+  ts.attr.names <- attributes(diag.sim.ts)$dimnames[[2]]
+  if (length(is.offset.term > 0)) {
+    ts.attr.names <- ts.attr.names[-is.offset.term]
+  }
+  ts.out <- data.frame(names = ts.attr.names,
                        targets = target.stats)
 
 
--- tests/testthat/test-diagnostics.R
+++ tests/testthat/test-diagnostics.R
@@ -19,6 +19,24 @@ est1 <- netest(nw,
                verbose = FALSE)
 
 
+## Model 2: Includes offset term
+n <- 50
+nw <- network.initialize(n, directed = FALSE)
+nw <- set.vertex.attribute(nw, "loc", rep(0:1, each = n/2))
+dissolution <- ~ offset(edges)
+duration <- 40
+coef.diss <- dissolution_coefs(dissolution, duration)
+formation <- ~ edges + offset(nodemix("loc", base=c(1, 3)))
+target.stats <- 15
+est2 <- netest(nw,
+              formation,
+              dissolution,
+              target.stats,
+              coef.diss,
+              coef.form = -Inf)
+
+
+
 # Tests -------------------------------------------------------------------
 
 test_that("netdx works for single simulations of edges only model", {
@@ -41,3 +59,17 @@ test_that("netdx works for reduced monitoring formula", {
   dx <- netdx(est1, nsims = 2, nsteps = 10, verbose = FALSE,
               nwstats.formula = ~meandeg)
   expect_is(dx, "netdx")
+})
+
+test_that("netdx for model with offset term", {
+  dx <- netdx(est2, nsims = 2, nsteps = 10, verbose = FALSE)
+  expect_is(dx, "netdx")
+})
+
+test_that("netdx for model with offset term and expanded formula", {
+  dx <- netdx(est2, nsims = 2, nsteps = 10, verbose = FALSE,
+              nwstats.formula = ~edges + meandeg + concurrent + nodematch("loc"))
+  expect_is(dx, "netdx")
+})
+
+
