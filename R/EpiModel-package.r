8eab38dec77503d81e8cd015dd7bab17f1b51cbd
Samuel Jenness <smjenness@gmail.com>
2014-09-06 21:01:46 -0700

    Update version and date in main doc file

--- R/EpiModel-package.r
+++ R/EpiModel-package.r
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #'    Package: \tab EpiModel\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 1.0.1\cr
-#'    Date: \tab 2014-07-25\cr
+#'    Version: \tab 1.1\cr
+#'    Date: \tab 2014-09-06\cr
 #'    License: \tab GPL (>= 2)\cr
 #'    LazyLoad: \tab yes\cr
 #' }
