82bf6275660f1fa1fd58c7f4d4ffdb6247f15bf9
Samuel Jenness <sjenness@uw.edu>
2015-04-03 20:12:53 -0700

    Add test for heterogeneous model

--- tests/testthat/test-netest.R
+++ tests/testthat/test-netest.R
@@ -1,8 +1,5 @@
 context("netest")
 
-
-# netest ------------------------------------------------------------------
-
 test_that("netest works for edges only model", {
   nw <- network.initialize(n = 50, directed = FALSE)
   est <- netest(
@@ -14,6 +11,7 @@ test_that("netest works for edges only model", {
     verbose = FALSE)
   expect_is(est, "netest")
 })
+
 
 test_that("netest works for edges + nodematch model", {
   nw <- network.initialize(n = 50, directed = FALSE)
@@ -30,18 +28,30 @@ test_that("netest works for edges + nodematch model", {
 
 
 test_that("netest works with offset.coef terms", {
-  mynet <- network.initialize(100, directed = FALSE)
-  mynet <- set.vertex.attribute(mynet, "role",
-                                c(rep("I", 10),
-                                  rep("V", 80),
-                                  rep("R", 10)))
-  mymodel <- netest(mynet,
-                    formation = ~ edges +
-                                  offset(nodematch('role', diff = TRUE, keep = 1:2)),
-                    coef.form = c(-Inf, -Inf),
-                    target.stats = c(40),
-                    dissolution = ~offset(edges),
-                    coef.diss = dissolution_coefs(~offset(edges), 52*2, 0.0009),
-                    verbose = FALSE)
-  expect_is(mymodel, "netest")
+  nw <- network.initialize(100, directed = FALSE)
+  nw <- set.vertex.attribute(nw, "role", rep(c("I", "V", "R"), c(10, 80, 10)))
+  est <- netest(nw,
+                formation = ~ edges +
+                              offset(nodematch('role', diff = TRUE, keep = 1:2)),
+                coef.form = c(-Inf, -Inf),
+                target.stats = c(40),
+                dissolution = ~offset(edges),
+                coef.diss = dissolution_coefs(~offset(edges), 52*2, 0.0009),
+                verbose = FALSE)
+  expect_is(est, "netest")
+})
+
+
+test_that("netest works for heterogeneous dissolutions", {
+  nw <- network.initialize(100, directed = FALSE)
+  nw <- set.vertex.attribute(nw, "race", rbinom(50, 1, 0.5))
+  est <- netest(
+    nw,
+    formation = ~edges + nodematch("race"),
+    dissolution = ~ edges + nodematch("race"),
+    target.stats = c(50, 20),
+    coef.diss = dissolution_coefs(~offset(edges) + offset(nodematch("race")), c(10, 20)),
+    verbose = FALSE
+  )
+  expect_is(est, "netest")
 })
