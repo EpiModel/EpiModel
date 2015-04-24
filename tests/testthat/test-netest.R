746ea35ae9a5008a7319fd7e0d55cdb0e2dbb462
Samuel Jenness <sjenness@uw.edu>
2015-04-23 20:25:13 -0700

    Add test for diss_check tests

--- tests/testthat/test-netest.R
+++ tests/testthat/test-netest.R
@@ -1,5 +1,8 @@
 context("netest")
 
+
+# Test formation models ---------------------------------------------------
+
 test_that("netest works for edges only model", {
   nw <- network.initialize(n = 50, directed = FALSE)
   est <- netest(
@@ -36,11 +39,14 @@ test_that("netest works with offset.coef terms", {
                 coef.form = c(-Inf, -Inf),
                 target.stats = c(40),
                 dissolution = ~offset(edges),
-                coef.diss = dissolution_coefs(~offset(edges), 52*2, 0.0009),
+                coef.diss = dissolution_coefs(~offset(edges), 52 * 2, 0.0009),
                 verbose = FALSE)
   expect_is(est, "netest")
 })
 
+
+
+# Test dissolution models -------------------------------------------------
 
 test_that("netest works for heterogeneous dissolutions", {
   nw <- network.initialize(100, directed = FALSE)
@@ -54,4 +60,23 @@ test_that("netest works for heterogeneous dissolutions", {
     verbose = FALSE
   )
   expect_is(est, "netest")
+})
+
+test_that("netest diss_check flags bad models", {
+  nw <- network.initialize(100, directed = FALSE)
+  nw <- set.vertex.attribute(nw, "race", rbinom(50, 1, 0.5))
+
+  formation <- ~edges + nodematch("race")
+  dissolution <- ~offset(edges) + offset(nodefactor("race"))
+  cd <- dissolution_coefs(dissolution, c(10, 20))
+  expect_error(netest(nw, formation, dissolution, target.stats = c(50, 20),
+               cd, verbose = FALSE),
+               "Dissolution model is not a subset of formation model.")
+
+  formation <- ~edges + nodematch("race", diff = TRUE)
+  dissolution <- ~offset(edges) + offset(nodematch("race"))
+  cd <- dissolution_coefs(dissolution, c(10, 20))
+  expect_error(netest(nw, formation, dissolution, target.stats = c(50, 20),
+                      cd, verbose = FALSE),
+               "Term options for one or more terms in dissolution model")
 })
