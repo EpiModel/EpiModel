49e46b9f39fca13261e61f58115c0bd7fbb7370f
Samuel Jenness <sjenness@uw.edu>
2015-07-02 09:22:38 -0700

    More imports updates

--- R/EpiModel-package.r
+++ R/EpiModel-package.r
@@ -111,6 +111,7 @@
 #' @name EpiModel-package
 #' @aliases EpiModel
 #' @import deSolve ergm network networkDynamic RColorBrewer tergm foreach doParallel
+#'         graphics stats utils
 #' @docType package
 #' @keywords package
 #'
--- R/net.mod.simnet.R
+++ R/net.mod.simnet.R
@@ -17,7 +17,7 @@ sim_nets <- function(x, nw, nsteps, control) {
 
   if (x$edapprox == FALSE) {
     suppressWarnings(
-      sim <- simulate(nw,
+      sim <- tergm::simulate(nw,
                       time.slices = nsteps,
                       monitor = control$nwstats.formula,
                       nsim = 1,
@@ -26,7 +26,7 @@ sim_nets <- function(x, nw, nsteps, control) {
                       control = control$set.control.stergm))
   } else {
     suppressWarnings(
-      sim <- simulate(nw,
+      sim <- tergm::simulate(nw,
                       formation = x$formation,
                       dissolution = x$coef.diss$dissolution,
                       coef.form = x$coef.form,
@@ -90,7 +90,7 @@ resim_nets <- function(dat, at) {
   # Network simulation
   if (anyActive > 0 & dat$control$depend == TRUE) {
     suppressWarnings(
-      dat$nw <- simulate(dat$nw,
+      dat$nw <- tergm::simulate(dat$nw,
                          formation = nwparam$formation,
                          dissolution = nwparam$coef.diss$dissolution,
                          coef.form = nwparam$coef.form,
