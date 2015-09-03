2c84c5cd1d23caa58d398cf5ca0a8c02a1cdbb98
Emily Beylerian <ebey@uw.edu>
2015-09-03 10:23:43 -0700

    basic model with netdx (#68)

--- inst/shiny/epinet/server.R
+++ inst/shiny/epinet/server.R
@@ -10,4 +10,41 @@ library(EpiModel)
 
 shinyServer(function(input, output) {
 
+  #Main reactive objects
+  net <- reactive({
+    network.initialize(n = input$num, directed = input$directed)
+  })
+  coef.diss <- reactive({
+    dissolution_coefs(dissolution = as.formula(input$dissolution),
+                      duration = input$dur)
+  })
+  fit <- reactive({
+    if(input$runMod == 0){return()}
+    isolate(
+      netest(net(), formation = as.formula(input$formation),
+             target.stats = input$form.targets,
+             coef.diss = coef.diss())
+    )
+  })
+  dxsim <- reactive({
+    if(input$runMod == 0){return()}
+    input$runDx
+    isolate(
+      netdx(fit(), nsims = input$dx.nsims, nsteps = input$dx.nsteps,
+            keep.tedgelist = FALSE)
+    )
+  })
+
+  #Output objects
+  output$modelsum <- renderPrint({
+    if(!is.null(fit())){
+      summary(fit())
+    }
+  })
+  output$dxplot <- renderPlot({
+    if(!is.null(dxsim())){
+      plot(dxsim(), type = input$dxtype)
+    }
+  })
+
 })
--- inst/shiny/epinet/ui.R
+++ inst/shiny/epinet/ui.R
@@ -10,7 +10,63 @@ library(EpiModel)
 
 shinyUI(fluidPage(
 
-  titlePanel("EpiModel: Network Models")
+  titlePanel("EpiModel: Network Models"),
 
+  sidebarLayout(
+    sidebarPanel(
 
+      h3("Instructions", style = "margin-top: 0px"),
+      helpText("Click Run Model after changing model parameters",
+               "or conditions."),
+      actionButton("runMod", "Run Model"),
+
+      h4("Initialize Network", style = "margin-top: 25px"),
+      numericInput("num",
+                   label = "Number of Nodes",
+                   value = 100,
+                   min = 0),
+      checkboxInput("directed",
+                    label = "Directed?",
+                    value = FALSE),
+
+      selectInput("formation",
+                  label = "Formation Formula",
+                  choices = c("~edges")),
+      numericInput("form.targets",
+                   label = "Target Statistics",
+                   value = 20),
+
+      selectInput("dissolution",
+                  label = "Dissolution Formula",
+                  choices = c("~offset(edges)")),
+      numericInput("dur",
+                   label = "Edge Durations",
+                   value = 90)
+
+    ), #End sidebarPanel
+
+    # Main panel
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Net Diagnostics",
+                 fluidRow(
+                   column(3, offset = 1,
+                          numericInput("dx.nsims",
+                                       label = "Simulations",
+                                       value = 5, min = 1)),
+                   column(3, numericInput("dx.nsteps",
+                                          label = "Time Steps per Sim",
+                                          value = 500, min = 1)),
+                   column(3, actionButton("runDx",
+                                          label = "Re-Run Diagnostics",
+                                          style = "margin-top: 25px;"))
+                 ),
+                 plotOutput("dxplot"),
+                 selectInput("dxtype",
+                             label = "Plot Type",
+                             choices = c("formation", "dissolution", "duration")),
+                 verbatimTextOutput("modelsum"))
+      )
+    )
+  )
 ))
