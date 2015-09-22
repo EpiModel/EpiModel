bf00f916eec53654d05c8f6603eff5188ccd5e24
Skye Bender-deMoll <skyebend@skyeome.net>
2015-09-22 16:54:19 -0700

    worked for the first time

--- R/dendo.R
+++ R/dendo.R
@@ -2,46 +2,46 @@
 require(ape)
 
 #' @title functions for converting to and from phylo class
-#' @examples 
- require(tsna)
- data(moodyContactSim)
- v10path<-tPath(moodyContactSim,v=10,start=0)
- 
- getChildren(10,17,v10path$previous)
- plot(v10path)
- as.phylo.tPath(moodyContactSim,v10path)
+#' @import ape
+# require(tsna)
+# data(moodyContactSim)
+# v10path<-tPath(moodyContactSim,v=10,start=0)
+# 
+# getChildren(10,17,v10path$previous)
+# plot(v10path)
+# as.phylo.tPath(moodyContactSim,v10path)
  
 
  
 # edgelist version 
-as.phylo.tPath<-function(net, path){
-
-  el<-cbind(path$previous,1:length(path$previous))
-  # remove the seed row
-  el<-el[path$previous!=0,]
-  # 'nodes' are all the vertices with decendents
-  nodes<-unique(path$previous[path$previous!=0])
-  # tips are all the vertices that are not nodes
-  tips<-setdiff(1:length(path$previous),nodes)
-  
-  ids<-c(tips,nodes)
-  
-  # remap numbering
-  el[,1]<-match(el[,1],ids)
-  el[,2]<-match(el[,2],ids)
-  
-  # delete the singles
-  
-  
-  out<-list()
-  out[['edge']]<-el
-  out[['Nnode']]<-length(nodes)  # number of non-tip nodes
-  out[['tip.label']]<-network.vertex.names(net)[tips]
-  out[['node.label']]<-network.vertex.names(net)[nodes]
-  out[['edge.length']]<-path$tdist
-  class(out)<-'phylo'
-  return(out)
-}
+# as.phylo.tPath<-function(net, path){
+# 
+#   el<-cbind(path$previous,1:length(path$previous))
+#   # remove the seed row
+#   el<-el[path$previous!=0,]
+#   # 'nodes' are all the vertices with decendents
+#   nodes<-unique(path$previous[path$previous!=0])
+#   # tips are all the vertices that are not nodes
+#   tips<-setdiff(1:length(path$previous),nodes)
+#   
+#   ids<-c(tips,nodes)
+#   
+#   # remap numbering
+#   el[,1]<-match(el[,1],ids)
+#   el[,2]<-match(el[,2],ids)
+#   
+#   # delete the singles
+#   
+#   
+#   out<-list()
+#   out[['edge']]<-el
+#   out[['Nnode']]<-length(nodes)  # number of non-tip nodes
+#   out[['tip.label']]<-network.vertex.names(net)[tips]
+#   out[['node.label']]<-network.vertex.names(net)[nodes]
+#   out[['edge.length']]<-path$tdist
+#   class(out)<-'phylo'
+#   return(out)
+# }
 
 # crawler version
 as.phylo.tPath<-function(net, path){
@@ -89,14 +89,17 @@ as.phylo.tPath<-function(net, path){
   return(out)
 }
 
+#' @title convert transmat infection tree into a phylo tree object
+#' @method as.phylo transmat
+#' @export as.phylo.transmat
 as.phylo.transmat<-function(tm){
   el<-cbind(tm$inf,tm$sus)
   #phylo doesn't like "non-splitting" nodes that have no children
   singleKidNodes<-as.numeric(names(which(table(el[,1])==1)))
   if (length(singleKidNodes)>0){
     singleKidsRows<-el[,1]%in%singleKidNodes
-    warning('removed row(s) ',paste(which(singleKidsRows)), ' corresponding to single child')
-    el<-el[!singleKidsRows,,drop=FALSE]
+    warning('found vertices ',paste(singleKidNodes,collapse = ','),' that create "non-splitting" phlo nodes ')
+    #el<-el[!singleKidsRows,,drop=FALSE]
   }
   
   origNodes<-unique(el[,1])
@@ -107,13 +110,19 @@ as.phylo.transmat<-function(tm){
 
   # find roots (infectors that never appear as sus)
   v<-setdiff(unique(el[,1]),unique(el[,2]))
+  if(length(v)>1){
+    warning('found multiple trees ',length(v),' not yet supported')
+  }
   # figure out the ordering such that the root
   # node will be one larger than the tip nodes
   phyloN<-length(origTips)+1
   while(length(v)>0){
     origIndex<-which(origNodes==v[1])
-    phyloNodes[origIndex]<-phyloN
-    phyloN<-phyloN+1
+    if(length(origIndex)>0){
+      # add the element on the list of phylo nodes
+      phyloNodes[origIndex]<-phyloN
+      phyloN<-phyloN+1
+    }
     kids<-el[el[,1]==v[1],2] # look up kids on the edgelist
     v<-c(v[-1],kids)
   }
@@ -133,66 +142,20 @@ as.phylo.transmat<-function(tm){
   return(out)
 }
 
+#' @title converting transmat infection tree into a network object
+#' @method as.network transmat
+#' @export as.network.transmat
 as.network.transmat<-function(tm){
   ids<-unique(c(tm$sus,tm$inf))
   # remap the ids to a new set
   tm$sus<-match(tm$sus,ids)
   tm$inf<-match(tm$inf,ids)
-  net<-network(cbind(tm$inf,tm$sus))
+  net<-network(cbind(tm$inf,tm$sus),matrix.type='edgelist')
   network.vertex.names(net)<-ids
   return(net)
 }
 
-
-# function to recursively loop through infection tree
-# recursive version
-getChildren<-function(v,parent,previous){
-  
-  vKids<-previous[previous[,1]==v,2]
-  if(length(vKids)>0){
-    # if the child has children, call deepr
-    vC<-lapply(vKids,function(vKid){
-      getChildren(vKid,parent=parent+1,previous=previous)  # look up my kids, with a new parent
-    })
-    el<-do.call(rbind,vC)
-    el<-rbind(el,cbind(parent,parent+1,-1)) # link my parent to my kids parent
-    #TODO: can't figure out how to include a parent with kids in the correct group
-    el<-rbind(el,cbind(parent,v,-2))
-  } 
-  else {
-   # if I have no children, just link me to my parent
-    el<-cbind(parent,v,-3)
-  }
-  return(el)
-}
-
-# sequential version
-getChildren<-function(v,parent,previous){
-  vKids<-previous[previous[,1]==v,2]
-  newNodes<-max(previous[,2])+1
-  # add a link to the root node?
-  el<-matrix(c(newNodes[1],v,0),ncol=3,byrow=TRUE)
-  while(length(vKids)>0){
-    # find which children have children
-    vC<-lapply(vKids,function(vKid){
-      previous[previous[,1]==vKid,2]  # look up my kids, with a new parent
-    })
-    # for each kid found...
-    for(kid in vC){
-      switch (length(kid),
-        1 =   el<-rbind(el,cbind(parent,v,-2)) # this is the end of branch, add with parent
-      )
-    }
-    
-    
-    el<-rbind(el,cbind(parent,parent+1,-1)) # link my parent to my kids parent
-    #TODO: can't figure out how to include a parent with kids in the correct group
-    el<-rbind(el,cbind(parent,v,-2))
-    el<-rbind(el,do.call(rbind,vC))
-  } 
-
-  return(el)
-}
+if(FALSE){
 
 
 phytest<-list(edge=matrix(c(5,4,
@@ -224,14 +187,12 @@ phytest<-list(edge=matrix(c(5,1,
 class(phytest)<-'phylo'
 plot(phytest,show.node.label=TRUE,use.edge.length = TRUE)
 
-cat('(10,(7,(c,d)));',file='test.tre')
-
-
+}
 
 
 
 ### example code
-
+transmat_example<-function(){
 library(EpiModel)
 nw <- network.initialize(n = 100, directed = FALSE)
 formation <- ~edges
@@ -245,4 +206,6 @@ control <- control.net(type = "SI", nsteps = 40, nsims = 1, verbose.int = 0, use
 mod1 <- netsim(est1, param, init, control)
 
 tm <- get_transmat(mod1)
+class(tm)<-c('transmat',class(tm))
 tm
+}
