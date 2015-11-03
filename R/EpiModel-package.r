ea69402060f2b26b3cf2729d8e035f89bf6ecb74
Samuel Jenness <sjenness@uw.edu>
2015-11-02 21:17:50 -0500

    Bump dates

--- R/EpiModel-package.r
+++ R/EpiModel-package.r
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #'    Package: \tab EpiModel\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 1.2.1\cr
-#'    Date: \tab 2015-07-22\cr
+#'    Version: \tab 1.2.2\cr
+#'    Date: \tab 2015-11-02\cr
 #'    License: \tab GPL (>= 2)\cr
 #'    LazyLoad: \tab yes\cr
 #' }
