304fc76e42eb1675d3f478f7d191297163a25bf8
Skye Bender-deMoll <skyebend@skyeome.net>
2016-09-20 20:13:36 -0700

    added some missing test params

--- tests/testthat/test-fastedgelist.R
+++ tests/testthat/test-fastedgelist.R
@@ -1,4 +1,5 @@
-
+require(testthat)
+require(EpiModel)
 
 # tests for evaluating the fast.edgelist mode and ensuring that its output matches
 context('fast.edgelist mode')
@@ -130,9 +131,9 @@ test_that('edges+nodematch(diff=TRUE) model works',{
   nw%v%'rolemode'<-rep_len(c('a','b','c'),network.size(nw))
   foo <- TRUE  # this is to test that evaluating formula args in calling environment works
                
-  valRange <- 2:3
+  #valRange <- 2:3
   # formation <- ~edges+offset(nodematch('rolemode',diff=foo,keep=valRange))  # this seems to run fine in manual testing, but not inside testthat, different environment evaluation?
-  formation <- ~edges+offset(nodematch('rolemode',diff=TRUE,keep=valRange))
+  formation <- ~edges+offset(nodematch('rolemode',diff=TRUE,keep=2:3))
   target.stats <- 50
   
   # calculate dissolution coefficient with death rate
@@ -341,8 +342,15 @@ test_that('edges+concurrent model works',{
   coef.diss <- dissolution_coefs(dissolution = ~offset(edges), duration = 20,
                                  d.rate = 0.0021)
   
+  param <- param.net(inf.prob = 0.3, 
+                     rec.rate = 0.02, 
+                     b.rate = 0.00, 
+                     ds.rate = 0.01, 
+                     di.rate = 0.0, 
+                     dr.rate = 0.0)
+  init<- init.net(i.num = 10,
+                  r.num = 0)
   # Reestimate the model with new coefficient
-  set.seed(1)
   est <- netest(nw, formation, target.stats, coef.diss, verbose = FALSE)
   
   models <- compareNetVsElmodel(est, 'Concurrent',param,init,numSteps = NUMSTEPS,numSims = NUMSIMS)
@@ -360,6 +368,14 @@ test_that('edges+nodefactor model works',{
   # calculate dissolution coefficient with death rate
   coef.diss <- dissolution_coefs(dissolution = ~offset(edges), duration = 20,
                                  d.rate = 0.0021)
+  param <- param.net(inf.prob = 0.3, 
+                     rec.rate = 0.02, 
+                     b.rate = 0.00, 
+                     ds.rate = 0.01, 
+                     di.rate = 0.0, 
+                     dr.rate = 0.0)
+  init<- init.net(i.num = 10,
+                  r.num = 0)
   
   # Reestimate the model with new coefficient
   est <- netest(nw, formation, target.stats, coef.diss, verbose = FALSE)
