d35ce640d0fe7a6ee754364cfe0ba3ea30299931
Kevin Weiss <kevin.weiss@emory.edu>
2017-10-19 15:49:19 -0400

    Move truncate_sim function to EpiModel from EMH

--- R/net.utils.R
+++ R/net.utils.R
@@ -957,3 +957,39 @@ get_degree <- function(x) {
   out <- tabulate(x, nbins = n)
   return(out)
 }
+
+#' @title Truncate Simulation Time Series
+#'
+#' @description Left-truncates a simulation epidemiological summary statistics and
+#'              network statistics at a specified time step.
+#'
+#' @param x Object of class \code{netsim} or \code{icm}.
+#' @param at Time step at which to left-truncate the time series.
+#'
+#' @details
+#' This function would be used when running a follow-up simulation from time steps
+#' \code{b} to \code{c} after a burnin period from time \code{a} to \code{b},
+#' where the final time window of interest for data analysis is \code{b} to \code{c}
+#' only.
+#'
+#' @export
+#'
+truncate_sim <- function(x, at) {
+
+  if (class(x) != "icm" && class(x) != "netsim") {
+    stop("x must be either an object of class icm or class netsim",
+         call. = FALSE)
+  }
+
+  rows <- at:(x$control$nsteps)
+
+  # epi
+  x$epi <- lapply(x$epi, function(r) r[rows, ])
+
+  # control settings
+  x$control$start <- 1
+  x$control$nsteps <- max(seq_along(rows))
+
+  return(x)
+}
+
