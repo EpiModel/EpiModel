800defaf75db46929ff5c3f503b5f9e1d91303ba
Samuel Jenness <smjenness@gmail.com>
2018-01-11 20:32:24 -0500

    Add truncate_sim example

--- R/net.utils.R
+++ R/net.utils.R
@@ -974,6 +974,23 @@ get_degree <- function(x) {
 #'
 #' @export
 #'
+#' @examples
+#' set.seed(12345)
+#' param <- param.icm(inf.prob = 0.2, act.rate = 0.25)
+#' init <- init.icm(s.num = 500, i.num = 1)
+#' control <- control.icm(type = "SI", nsteps = 200, nsims = 1)
+#' mod1 <- icm(param, init, control)
+#' df <- as.data.frame(mod1, out = "vals")
+#' print(df)
+#' plot(mod1)
+#' mod1$control$nsteps
+#'
+#' mod2 <- truncate_sim(mod1, at = 150)
+#' df2 <- as.data.frame(mod2)
+#' print(df2)
+#' plot(mod2)
+#' mod2$control$nsteps
+#'
 truncate_sim <- function(x, at) {
 
   if (class(x) != "icm" && class(x) != "netsim") {
