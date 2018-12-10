59d442c888fd3e70db4299a177d7e85d9fbca91c
smjenness <smjenness@gmail.com>
2018-12-10 13:06:11 -0500

    Remove extraneous verbose output

--- R/verbose.R
+++ R/verbose.R
@@ -160,58 +160,8 @@ verbose.net <- function(x, type, s = 1, at = 2) {
         cat("\n----------------------------")
         cat("\nSimulation: ", s, "/", x$control$nsims, sep = "")
         cat("\nTimestep: ", at, "/", x$control$nsteps, sep = "")
-        if (x$param$modes == 1) {
-          cat("\nIncidence:", x$epi$si.flow[at])
-        }
-        if (x$param$modes == 2) {
-          cat("\nIncidence:", x$epi$si.flow[at] + x$epi$si.flow.m2[at])
-        }
-        if (x$control$type == "SIR") {
-          if (x$param$modes == 1) {
-            cat("\nRecoveries:", x$epi$ir.flow[at])
-          }
-          if (x$param$modes == 2) {
-            cat("\nRecoveries:", x$epi$ir.flow[at] +
-                                 x$epi$ir.flow.m2[at])
-          }
-        }
-        if (x$control$type == "SIS") {
-          if (x$param$modes == 1) {
-            cat("\nRecoveries:", x$epi$is.flow[at])
-          }
-          if (x$param$modes == 2) {
-            cat("\nRecoveries:", x$epi$is.flow[at] +
-                                 x$epi$is.flow.m2[at])
-          }
-        }
-        if (x$param$modes == 1) {
-          cat("\nPrevalence:", x$epi$i.num[at])
-        }
-        if (x$param$modes == 2) {
-          cat("\nPrevalence:", x$epi$i.num[at] + x$epi$i.num.m2[at])
-        }
-        cat("\nPopulation:", sum(x$attr$active == 1))
-        if (x$param$vital == TRUE) {
-          if (x$param$modes == 1) {
-            cat("\nArrivals:", x$epi$a.flow[at])
-            cat("\nDepartures, susceptibles:", x$epi$ds.flow[at])
-            cat("\nDepartures, infecteds:", x$epi$di.flow[at])
-            if (x$control$type == "SIR") {
-              cat("\nDepartures, recovered:", x$epi$dr.flow[at])
-            }
-          }
-          if (x$param$modes == 2) {
-            cat("\nArrivals:", x$epi$a.flow[at] + x$epi$a.flow.m2[at])
-            cat("\nDepartures, susceptible:", x$epi$ds.flow[at] +
-                                          x$epi$ds.flow.m2[at])
-            cat("\nDepartures, infected:", x$epi$di.flow[at] +
-                                       x$epi$di.flow.m2[at])
-            if (x$control$type == "SIR") {
-              cat("\nDepartures, recovered:", x$epi$dr.flow[at] +
-                                          x$epi$dr.flow.m2[at])
-            }
-          }
-        }
+        cat("\nPrevalence:", sum(x$attr$status == "i", na.rm = TRUE))
+        cat("\nPopulation Size:", sum(x$attr$active == 1))
         cat("\n----------------------------")
       }
     }
