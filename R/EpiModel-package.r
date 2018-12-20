8dd72089c109d4802424d68cd6506bfc2803fec1
smjenness <smjenness@gmail.com>
2018-12-20 10:42:01 -0500

    Bump version/date

--- R/EpiModel-package.r
+++ R/EpiModel-package.r
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #'    Package: \tab EpiModel\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 1.7.2\cr
-#'    Date: \tab 2018-12-18\cr
+#'    Version: \tab 1.7.3\cr
+#'    Date: \tab 2018-12-20\cr
 #'    License: \tab GPL-3\cr
 #'    LazyLoad: \tab yes\cr
 #' }
