3c739ef9db96692e95615517bf1fea7bad9bf6a1
Connor Van Meter <connor.m.van.meter@emory.edu>
2019-01-24 10:22:28 -0500

    Update netdx.R
    Resolves issue 300.
    Error message when netest containing a cross-sectional ergm sent to netdx(dynamic=TRUE).

--- R/netdx.R
+++ R/netdx.R
@@ -119,6 +119,11 @@ netdx <- function(x, nsims = 1, dynamic = TRUE, nsteps, nwstats.formula = "forma
     stop("Specify number of time steps with nsteps", call. = FALSE)
   }
 
+  if (x$coef.diss$duration == 1 & dynamic == TRUE) {
+    stop("Running dynamic diagnostics on a cross-sectional ERGM (duration = 1) is not possible. \nSet netdx parameter 'dynamic' to 'FALSE'",
+         call. = FALSE)
+  }
+
   if (dynamic == FALSE && nwstats.formula == "formation") {
     nwstats.formula <- x$formation
   }
