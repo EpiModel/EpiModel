3b31cfb72a36dab27849512ef0b83008a01fd5ae
Shawnee M. Anderson <shawnee.m.anderson@gmail.com>
2019-02-21 10:58:04 -0500

    andsv2/EpiModel/EpiModel-MIDAS#39 print.netsim reverse commit.

--- R/print.r
+++ R/print.r
@@ -36,30 +36,37 @@ print.dcm <- function(x, ...) {
 
 
 #' @export
-print.icm <- function(x, ...) {
-
+print.dcm <- function(x, ...) {
+  
+  # New model
+  new.mod <- ifelse(!is.null(x$control$new.mod), TRUE, FALSE)
+  
   cat("EpiModel Simulation")
   cat("\n=======================")
   cat("\nModel class:", class(x))
-
+  
   cat("\n\nSimulation Summary")
   cat("\n-----------------------")
-  cat("\nModel type:", x$control$type)
-  cat("\nNo. simulations:", x$control$nsims)
+  if (new.mod == FALSE) {
+    cat("\nModel type:", x$control$type)
+  }
+  cat("\nNo. runs:", x$control$nruns)
   cat("\nNo. time steps:", x$control$nsteps)
-  cat("\nNo. groups:", x$param$groups)
-
+  if (new.mod == FALSE) {
+    cat("\nNo. groups:", x$param$groups)
+  }
+  
   cat("\n\nModel Parameters")
   cat("\n-----------------------\n")
   pToPrint <- which(!(names(x$param) %in% c("groups", "vital")))
   for (i in pToPrint) {
     cat(names(x$param)[i], "=", x$param[[i]], fill = 60)
   }
-
+  
   cat("\nModel Output")
   cat("\n-----------------------")
   cat("\nVariables:", names(x$epi), fill = 60)
-
+  
   invisible()
 }
 
