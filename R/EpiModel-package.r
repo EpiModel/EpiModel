6159ba229da6e5899fc7472a3c518d2edfe29394
Samuel Jenness <smjenness@gmail.com>
2019-08-29 11:24:14 -0400

    Bump dates

--- R/EpiModel-package.r
+++ R/EpiModel-package.r
@@ -5,7 +5,7 @@
 #'    Package: \tab EpiModel\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 1.7.3\cr
-#'    Date: \tab 2019-04-24\cr
+#'    Date: \tab 2019-08-29\cr
 #'    License: \tab GPL-3\cr
 #'    LazyLoad: \tab yes\cr
 #' }
