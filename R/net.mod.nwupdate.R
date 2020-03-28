0ebb522a8351c985702922cc5f3ab19e7fcea263
Samuel Jenness <smjenness@gmail.com>
2020-03-28 11:13:36 -0400

    Remove param$vital block from nwupdate.net
    [skip ci]

--- R/net.mod.nwupdate.R
+++ R/net.mod.nwupdate.R
@@ -15,90 +15,87 @@ nwupdate.net <- function(dat, at) {
   groups <- dat$param$groups
   tL <- dat$control$tergmLite
 
-  if (dat$param$vital != FALSE) {
+  ## Arrivals
+  if (tL == FALSE) {
+    if (groups == 1) {
+      nArrivals <- dat$epi$a.flow[at]
+    } else {
+      nArrivals <- c(dat$epi$a.flow[at], dat$epi$a.flow.g2[at])
+    }
+    if (sum(nArrivals) > 0) {
+      nCurr <- network.size(dat$nw)
+      dat$nw <- add.vertices(dat$nw, nv = sum(nArrivals))
+      newNodes <- (nCurr + 1):(nCurr + sum(nArrivals))
+      dat$nw <- activate.vertices(dat$nw, onset = at, terminus = Inf, v = newNodes)
+      # TODO: not updating group on the nw like this
+      # if (length(nArrivals) > 1) {
+      #   dat$nw <- set.vertex.attribute(dat$nw, "group",
+      #                                  rep(1:2, c(nArrivals[1], nArrivals[2])),
+      #                                  newNodes)
+      # }
 
-    ## Arrivals
-    if (tL == FALSE) {
+      # Set attributes on nw
+      nwterms <- dat$temp$nwterms
+      if (!is.null(nwterms)) {
+        curr.tab <- get_attr_prop(dat, nwterms)
+        if (length(curr.tab) > 0) {
+          dat <- auto_update_attr(dat, newNodes, dat$control$attr.rules,
+                                  curr.tab, dat$temp$t1.tab)
+        }
+
+        # Save any val on attr
+        if (!is.null(nwterms)) {
+          # TODO: remove this
+          # dat <- copy_toall_attr(dat, at)
+          if (length(unique(sapply(dat$attr, length))) != 1) {
+            stop("Attribute list of unequal length. Check arrivals.net module.")
+          }
+        }
+        dat$nw <- activate.vertex.attribute(dat$nw, prefix = "testatus",
+                                            value = dat$attr$status[newNodes],
+                                            onset = at, terminus = Inf,
+                                            v = newNodes)
+      }
+    }
+  }
+
+  if (tL == TRUE) {
+    nwterms <- dat$temp$nwterms
+    if (!is.null(nwterms)) {
+      curr.tab <- get_attr_prop(dat, nwterms)
+
       if (groups == 1) {
         nArrivals <- dat$epi$a.flow[at]
       } else {
         nArrivals <- c(dat$epi$a.flow[at], dat$epi$a.flow.g2[at])
       }
       if (sum(nArrivals) > 0) {
-        nCurr <- network.size(dat$nw)
-        dat$nw <- add.vertices(dat$nw, nv = sum(nArrivals))
-        newNodes <- (nCurr + 1):(nCurr + sum(nArrivals))
-        dat$nw <- activate.vertices(dat$nw, onset = at, terminus = Inf, v = newNodes)
-        # TODO: not updating group on the nw like this
-        # if (length(nArrivals) > 1) {
-        #   dat$nw <- set.vertex.attribute(dat$nw, "group",
-        #                                  rep(1:2, c(nArrivals[1], nArrivals[2])),
-        #                                  newNodes)
-        # }
+        nCurr <- sum(dat$attr$active == 1)
+        dat$el[[1]] <- add_vertices(dat$el[[1]], nv = sum(nArrivals))
+        dat <- auto_update_attr(dat, nArrivals, dat$control$attr.rules,
+                                curr.tab, dat$temp$t1.tab)
 
-        # Set attributes on nw
-        nwterms <- dat$temp$nwterms
-        if (!is.null(nwterms)) {
-          curr.tab <- get_attr_prop(dat, nwterms)
-          if (length(curr.tab) > 0) {
-            dat <- auto_update_attr(dat, newNodes, dat$control$attr.rules,
-                                    curr.tab, dat$temp$t1.tab)
-          }
-
-          # Save any val on attr
-          if (!is.null(nwterms)) {
-            # TODO: remove this
-            # dat <- copy_toall_attr(dat, at)
-            if (length(unique(sapply(dat$attr, length))) != 1) {
-              stop("Attribute list of unequal length. Check arrivals.net module.")
-            }
-          }
-          dat$nw <- activate.vertex.attribute(dat$nw, prefix = "testatus",
-                                              value = dat$attr$status[newNodes],
-                                              onset = at, terminus = Inf,
-                                              v = newNodes)
-        }
       }
     }
-
-    if (tL == TRUE) {
-      nwterms <- dat$temp$nwterms
-      if (!is.null(nwterms)) {
-        curr.tab <- get_attr_prop(dat, nwterms)
-
-        if (groups == 1) {
-          nArrivals <- dat$epi$a.flow[at]
-        } else {
-          nArrivals <- c(dat$epi$a.flow[at], dat$epi$a.flow.g2[at])
-        }
-        if (sum(nArrivals) > 0) {
-          nCurr <- sum(dat$attr$active == 1)
-          dat$el[[1]] <- add_vertices(dat$el[[1]], nv = sum(nArrivals))
-          dat <- auto_update_attr(dat, nArrivals, dat$control$attr.rules,
-                                  curr.tab, dat$temp$t1.tab)
-
-        }
-      }
-    }
-
-
-    ## Departures
-    if (tL == FALSE) {
-      inactive <- which(dat$attr$active == 0 & dat$attr$exitTime == at)
-      if (length(inactive) > 0) {
-        dat$nw <- deactivate.vertices(dat$nw, onset = at, terminus = Inf,
-                                      v = inactive, deactivate.edges = TRUE)
-      }
-    }
-    if (tL == TRUE) {
-      inactive <- which(dat$attr$active == 0 & dat$attr$exitTime == at)
-      if (length(inactive) > 0) {
-        dat$attr <- deleteAttr(dat$attr, inactive)
-        dat$el[[1]] <- delete_vertices(dat$el[[1]], inactive)
-      }
-    }
-
   }
+
+
+  ## Departures
+  if (tL == FALSE) {
+    inactive <- which(dat$attr$active == 0 & dat$attr$exitTime == at)
+    if (length(inactive) > 0) {
+      dat$nw <- deactivate.vertices(dat$nw, onset = at, terminus = Inf,
+                                    v = inactive, deactivate.edges = TRUE)
+    }
+  }
+  if (tL == TRUE) {
+    inactive <- which(dat$attr$active == 0 & dat$attr$exitTime == at)
+    if (length(inactive) > 0) {
+      dat$attr <- deleteAttr(dat$attr, inactive)
+      dat$el[[1]] <- delete_vertices(dat$el[[1]], inactive)
+    }
+  }
+
 
   ## Recovery
   if (tL == FALSE) {
@@ -129,8 +126,6 @@ nwupdate.net <- function(dat, at) {
       dat$nw <- set.vertex.attribute(dat$nw, "status", dat$attr$status)
     }
   }
-
-
 
 
   ## Output
