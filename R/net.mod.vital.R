cc738987553303e41014ae01235b96c621f7e575
Shawnee M. Anderson <shawnee.m.anderson@gmail.com>
2020-04-14 01:27:11 -0400

    Update to arrival/departure functions using get/set helper functions.
    [skip ci]

--- R/net.mod.vital.R
+++ R/net.mod.vital.R
@@ -17,68 +17,75 @@ departures.net <- function(dat, at) {
   if (dat$param$vital == FALSE) {
     return(dat)
   }
-  type <- dat$control$type
+  type <- get_control(dat, "type")
+
+  active <- get_attr(dat, "active")
+  status <- get_attr(dat, "status")
+  exitTime <- get_attr(dat, "exitTime")
 
   # Susceptible departures ------------------------------------------------------
   nDepartures.sus <- 0
-  idsElig.sus <- which(dat$attr$active == 1 & dat$attr$status == "s")
+  idsElig.sus <- which(active == 1 & status == "s")
   nElig.sus <- length(idsElig.sus)
   if (nElig.sus > 0) {
-    rates.sus <- dat$param$ds.rate
+    rates.sus <- get_param(dat, "ds.rate")
     vecDepartures.sus <- which(rbinom(nElig.sus, 1, rates.sus) == 1)
     if (length(vecDepartures.sus) > 0) {
       idsDpt.sus <- idsElig.sus[vecDepartures.sus]
       nDepartures.sus <- length(idsDpt.sus)
-      dat$attr$active[idsDpt.sus] <- 0
-      dat$attr$exitTime[idsDpt.sus] <- at
+      active[idsDpt.sus] <- 0
+      exitTime[idsDpt.sus] <- at
     }
   }
 
   # Infected departures ---------------------------------------------------------
   nDepartures.inf <- 0
-  idsElig.inf <- which(dat$attr$active == 1 & dat$attr$status == "i")
+  idsElig.inf <- which(active == 1 & status == "i")
   nElig.inf <- length(idsElig.inf)
   if (nElig.inf > 0) {
-    rates.inf <- dat$param$di.rate
+    rates.inf <- get_param(dat, "di.rate")
     vecDepartures.inf <- which(rbinom(nElig.inf, 1, rates.inf) == 1)
     if (length(vecDepartures.inf) > 0) {
       idsDpt.inf <- idsElig.inf[vecDepartures.inf]
       nDepartures.inf <- length(idsDpt.inf)
-      dat$attr$active[idsDpt.inf] <- 0
-      dat$attr$exitTime[idsDpt.inf] <- at
+      active[idsDpt.inf] <- 0
+      exitTime[idsDpt.inf] <- at
     }
   }
 
   # Recovered departures --------------------------------------------------------
   if (type == "SIR") {
     nDepartures.rec <- 0
-    idsElig.rec <- which(dat$attr$active == 1 & dat$attr$status == "r")
+    idsElig.rec <- which(active == 1 & status == "r")
     nElig.rec <- length(idsElig.rec)
     if (nElig.rec > 0) {
-      rates.rec <- dat$param$dr.rate
+      rates.rec <- get_param(dat, "dr.rate")
       vecDepartures.rec <- which(rbinom(nElig.rec, 1, rates.rec) == 1)
       if (length(vecDepartures.rec) > 0) {
         idsDpt.rec <- idsElig.rec[vecDepartures.rec]
         nDepartures.rec <- length(idsDpt.rec)
-        dat$attr$active[idsDpt.rec] <- 0
-        dat$attr$exitTime[idsDpt.rec] <- at
+        active[idsDpt.rec] <- 0
+        exitTime[idsDpt.rec] <- at
       }
     }
   }
 
   # Output ------------------------------------------------------------------
 
+  dat <- set_attr(dat, "active", active)
+  dat <- set_attr(dat, "exitTime", exitTime)
+
   if (at == 2) {
-    dat$epi$ds.flow <- c(0, nDepartures.sus)
-    dat$epi$di.flow <- c(0, nDepartures.inf)
+    dat <- set_epi(dat, "ds.flow", c(0, nDepartures.sus))
+    dat <- set_epi(dat, "di.flow", c(0, nDepartures.inf))
     if (type == "SIR") {
-      dat$epi$dr.flow <- c(0, nDepartures.rec)
+      dat <- set_epi(dat, "dr.flow", c(0, nDepartures.rec))
     }
   } else {
-    dat$epi$ds.flow[at] <- nDepartures.sus
-    dat$epi$di.flow[at] <- nDepartures.inf
+    dat <- set_epi_at(dat, "ds.flow", at, nDepartures.sus)
+    dat <- set_epi_at(dat, "di.flow", at, nDepartures.inf)
     if (type == "SIR") {
-      dat$epi$dr.flow[at] <- nDepartures.rec
+      dat <- set_epi_at(dat, "dr.flow", at, nDepartures.rec)
     }
   }
 
@@ -103,32 +110,39 @@ departures.net <- function(dat, at) {
 arrivals.net <- function(dat, at) {
 
   # Conditions --------------------------------------------------------------
-  if (dat$param$vital == FALSE) {
+  vital <- get_param(dat, "vital")
+  if (vital == FALSE) {
     return(dat)
   }
 
   # Variables ---------------------------------------------------------------
-  a.rate <- dat$param$a.rate
-  nOld <- dat$epi$num[at - 1]
+  a.rate <- get_param(dat, "a.rate")
+  index <- at-1
+  nOld <- get_epi(dat, "num", index)
   nArrivals <- 0
 
   # Add Nodes ---------------------------------------------------------------
   if (nOld > 0) {
     nArrivals <- sum(rbinom(nOld, 1, a.rate))
     if (nArrivals > 0) {
-      dat$attr$status <- c(dat$attr$status, rep("s", nArrivals))
-      dat$attr$active <- c(dat$attr$active, rep(1, nArrivals))
-      dat$attr$infTime <- c(dat$attr$infTime, rep(NA, nArrivals))
-      dat$attr$entrTime <- c(dat$attr$entrTime, rep(at, nArrivals))
-      dat$attr$exitTime <- c(dat$attr$exitTime, rep(NA, nArrivals))
+      dat <- set_attr(dat, "status", c(get_attr(dat, "status"),
+                                       rep("s", nArrivals)))
+      dat <- set_attr(dat, "active", c(get_attr(dat, "active"),
+                                       rep(1, nArrivals)))
+      dat <- set_attr(dat, "infTime", c(get_attr(dat, "infTime"),
+                                        rep(NA, nArrivals)))
+      dat <- set_attr(dat, "entrTime", c(get_attr(dat, "entrTime"),
+                                         rep(NA, nArrivals)))
+      dat <- set_attr(dat, "exitTime", c(get_attr(dat, "exitTime"),
+                                         rep(NA, nArrivals)))
     }
   }
 
   # Output ------------------------------------------------------------------
   if (at == 2) {
-    dat$epi$a.flow <- c(0, nArrivals)
+    dat <- set_epi(dat, "a.flow", at, c(0,nArrivals))
   } else {
-    dat$epi$a.flow[at] <- nArrivals
+    dat <- set_epi_at(dat, "a.flow", at, nArrivals)
   }
 
   return(dat)
@@ -151,92 +165,104 @@ arrivals.net <- function(dat, at) {
 departures.2g.net <- function(dat, at) {
 
   # Conditions --------------------------------------------------------------
-  if (dat$param$vital == FALSE) {
+  vital <- get_param(dat, "vital")
+  if (vital == FALSE) {
     return(dat)
   }
 
   # Variables ---------------------------------------------------------------
-  group <- dat$attr$group
-  type <- dat$control$type
+
+  type <- get_control(dat, "type")
+
+  active <- get_attr(dat, "active")
+  status <- get_attr(dat, "status")
+  exitTime <- get_attr(dat, "exitTime")
+  group <- get_attr(dat, "group")
 
   # Susceptible departures ------------------------------------------------------
   nDepartures.sus <- nDeparturesG2.sus <- 0
-  idsElig.sus <- which(dat$attr$active == 1 & dat$attr$status == "s")
+  idsElig.sus <- which(active == 1 & status == "s")
   nElig.sus <- length(idsElig.sus)
   if (nElig.sus > 0) {
     gElig.sus <- group[idsElig.sus]
-    rates.sus <- c(dat$param$ds.rate, dat$param$ds.rate.g2)
+    rates.sus <- c(get_param(dat, "ds.rate"), get_param(dat, "ds.rate.g2"))
     ratesElig.sus <- rates.sus[gElig.sus]
     vecDepartures.sus <- which(rbinom(nElig.sus, 1, ratesElig.sus) == 1)
     if (length(vecDepartures.sus) > 0) {
       idsDpt.sus <- idsElig.sus[vecDepartures.sus]
       nDepartures.sus <- sum(group[idsDpt.sus] == 1)
       nDeparturesG2.sus <- sum(group[idsDpt.sus] == 2)
-      dat$attr$active[idsDpt.sus] <- 0
-      dat$attr$exitTime[idsDpt.sus] <- at
+      active[idsDpt.sus] <- 0
+      exitTime[idsDpt.sus] <- at
     }
   }
 
   # Infected departures ---------------------------------------------------------
   nDepartures.inf <- nDeparturesG2.inf <- 0
-  idsElig.inf <- which(dat$attr$active == 1 & dat$attr$status == "i")
+  idsElig.inf <- which(active == 1 & status == "i")
   nElig.inf <- length(idsElig.inf)
   if (nElig.inf > 0) {
     gElig.inf <- group[idsElig.inf]
-    rates.inf <- c(dat$param$di.rate, dat$param$di.rate.g2)
+    rates.inf <- c(get_param(dat, "di.rate"), get_param(dat, "di.rate.g2"))
     ratesElig.inf <- rates.inf[gElig.inf]
     vecDepartures.inf <- which(rbinom(nElig.inf, 1, ratesElig.inf) == 1)
     if (length(vecDepartures.inf) > 0) {
       idsDpt.inf <- idsElig.inf[vecDepartures.inf]
       nDepartures.inf <- sum(group[idsDpt.inf] == 1)
       nDeparturesG2.inf <- sum(group[idsDpt.inf] == 2)
-      dat$attr$active[idsDpt.inf] <- 0
-      dat$attr$exitTime[idsDpt.inf] <- at
+      active[idsDpt.inf] <- 0
+      exitTime[idsDpt.inf] <- at
     }
   }
 
   # Recovered departures --------------------------------------------------------
   if (type == "SIR") {
     nDepartures.rec <- nDeparturesG2.rec <- 0
-    idsElig.rec <- which(dat$attr$active == 1 & dat$attr$status == "r")
+    idsElig.rec <- which(active == 1 & status == "r")
     nElig.rec <- length(idsElig.rec)
     if (nElig.rec > 0) {
       gElig.rec <- group[idsElig.rec]
-      rates.rec <- c(dat$param$dr.rate, dat$param$dr.rate.g2)
+      rates.rec <- c(get_param(dat, "dr.rate"), get_param(dat, "dr.rate.g2"))
       ratesElig.rec <- rates.rec[gElig.rec]
       vecDepartures.rec <- which(rbinom(nElig.rec, 1, ratesElig.rec) == 1)
       if (length(vecDepartures.rec) > 0) {
         idsDpt.rec <- idsElig.rec[vecDepartures.rec]
         nDepartures.rec <- sum(group[idsDpt.rec] == 1)
         nDeparturesG2.rec <- sum(group[idsDpt.rec] == 2)
-        dat$attr$active[idsDpt.rec] <- 0
-        dat$attr$exitTime[idsDpt.rec] <- at
+        active[idsDpt.inf] <- 0
+        exitTime[idsDpt.inf] <- at
       }
     }
   }
 
   # Output ------------------------------------------------------------------
   if (at == 2) {
-    dat$epi$ds.flow <- c(0, nDepartures.sus)
-    dat$epi$di.flow <- c(0, nDepartures.inf)
+    dat <- set_epi(dat, "di.flow", c(0, nDepartures.inf))
+    dat <- set_epi(dat, "ds.flow", c(0, nDepartures.sus))
     if (type == "SIR") {
-      dat$epi$dr.flow <- c(0, nDepartures.rec)
+      dat <- set_epi(dat, "dr.flow", c(0, nDepartures.rec))
     }
-    dat$epi$ds.flow.g2 <- c(0, nDeparturesG2.sus)
-    dat$epi$di.flow.g2 <- c(0, nDeparturesG2.inf)
+    dat <- set_epi(dat, "di.flow.g2", c(0, nDeparturesG2.inf))
+    dat <- set_epi(dat, "ds.flow.g2", c(0, nDeparturesG2.sus))
     if (type == "SIR") {
-      dat$epi$dr.flow.g2 <- c(0, nDeparturesG2.rec)
+      dat <- set_epi(dat, "dr.flow.g2", c(0, nDeparturesG2.rec))
     }
   } else {
-    dat$epi$ds.flow[at] <- nDepartures.sus
-    dat$epi$di.flow[at] <- nDepartures.inf
+    dat <- set_epi_at(dat, "ds.flow", at, c(get_epi_list(dat, "ds.flow"),
+                                            nDepartures.sus))
+    dat <- set_epi_at(dat, "di.flow", at, c(get_epi_list(dat, "di.,flow"),
+                                            nDepartures.inf))
     if (type == "SIR") {
-      dat$epi$dr.flow[at] <- nDepartures.rec
+      dat <- set_epi_at(dat, "dr.flow", at, c(get_epi_list(dat, "dr.flow"),
+                                              nDepartures.rec))
     }
-    dat$epi$ds.flow.g2[at] <- nDeparturesG2.sus
-    dat$epi$di.flow.g2[at] <- nDeparturesG2.inf
+    dat <- set_epi_at(dat, "ds.flow.g2", at, c(get_epi_list(dat, "ds.flow.g2"),
+                                               nDepartures.sus))
+    dat <- set_epi_at(dat, "di.flow.g2", at, c(get_epi_list(dat, "di.,flow.g2"),
+                                               nDepartures.inf))
     if (type == "SIR") {
-      dat$epi$dr.flow.g2[at] <- nDeparturesG2.rec
+      dat <- set_epi_at(dat, "dr.flow.g2", at, c(get_epi_list(dat, "dr.flow.g2"),
+                                                 nDepartures.rec))
     }
   }
   return(dat)
@@ -260,17 +286,18 @@ departures.2g.net <- function(dat, at) {
 arrivals.2g.net <- function(dat, at) {
 
   # Conditions --------------------------------------------------------------
-  if (dat$param$vital == FALSE) {
+  vital <- get_param(dat, "vital")
+  if (vital == FALSE) {
     return(dat)
   }
 
   # Variables ---------------------------------------------------------------
-  a.rate <- dat$param$a.rate
-  a.rate.g2 <- dat$param$a.rate.g2
-  nOld <- dat$epi$num[at - 1]
-  nOldG2 <- dat$epi$num.g2[at - 1]
-  tergmLite <- dat$control$tergmLite
-
+  a.rate <- get_param(dat, "a.rate")
+  a.rate.g2 <- get_param(dat, "a.rate.g2")
+  index <- at-1
+  nOld <- get_epi(dat, "num", index)
+  nOldG2 <- get_epi(dat, "num.g2", index)
+  tergmLite <- get_control(dat, "tergmLite")
   totArr <- nArrivals <- nArrivalsG2 <- 0
   newNodes <- newNodesG2 <- NULL
 
@@ -288,25 +315,31 @@ arrivals.2g.net <- function(dat, at) {
     }
 
     if (totArr > 0) {
-      nCurr <- length(dat$attr$active)
+      nCurr <- length(get_attr(dat, "active"))
       newNodes <- (nCurr + 1):(nCurr + totArr)
-      dat$attr$group <- c(dat$attr$group, c(rep(1, nArrivals),
-                                            rep(2, nArrivalsG2)))
-      dat$attr$status <- c(dat$attr$status, rep("s", totArr))
-      dat$attr$active <- c(dat$attr$active, rep(1, totArr))
-      dat$attr$infTime <- c(dat$attr$infTime, rep(NA, totArr))
-      dat$attr$entrTime <- c(dat$attr$entrTime, rep(at, totArr))
-      dat$attr$exitTime <- c(dat$attr$exitTime, rep(NA, totArr))
+      dat <- set_attr(dat, "group", c(get_attr(dat, "group"),
+                                      rep(1, nArrivals),
+                                      rep(2, nArrivalsG2)))
+      dat <- set_attr(dat, "status", c(get_attr(dat, "status"),
+                                       rep("s", totArr)))
+      dat <- set_attr(dat, "active", c(get_attr(dat, "active"),
+                                       rep(1, totArr)))
+      dat <- set_attr(dat, "infTime", c(get_attr(dat, "infTime"),
+                                        rep(NA, totArr)))
+      dat <- set_attr(dat, "entrTime", c(get_attr(dat, "entrTime"),
+                                         rep(at, totArr)))
+      dat <- set_attr(dat, "exitTime", c(get_attr(dat, "exitTime"),
+                                         rep(NA, totArr)))
     }
   }
 
   # Output ------------------------------------------------------------------
   if (at == 2) {
-    dat$epi$a.flow <- c(0, nArrivals)
-    dat$epi$a.flow.g2 <- c(0, nArrivalsG2)
+    dat <- set_epi(dat, "a.flow", c(0, nArrivals))
+    dat <- set_epi(dat, "a.flow.g2", c(0, nArrivalsG2))
   } else {
-    dat$epi$a.flow[at] <- nArrivals
-    dat$epi$a.flow.g2[at] <- nArrivalsG2
+    dat <- set_epi_at(dat, "a.flow", at, nArrivals)
+    dat <- set_epi_at(dat, "a.flow.g2",at, nArrivalsG2)
   }
 
   return(dat)
