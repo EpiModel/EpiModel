62d0f44ca9758340f559a3ac6f351e162a4939bc
Shawnee M. Anderson <shawnee.m.anderson@gmail.com>
2020-04-29 02:01:33 -0400

    Initial pass at set_network helper function.

--- R/get.R
+++ R/get.R
@@ -116,6 +116,50 @@ get_network <- function(x, sim = 1, network = 1, collapse = FALSE, at) {
   return(out)
 }
 
+#' @title Set Network in Master Object List
+#'
+#' @description Helper function that sets an object of class \code{network}
+#'              onto a master data list.
+#' @param dat Master data object.
+#' @param nw  An object of class \code{network}.
+#' @param index Where in the network sublist to save.
+#'
+#' @return A master data object with specified network saved on to sublist \code{nw}.
+#'
+#' @keywords network
+#' @export
+#'
+#' @examples
+#' nw <- network.initialize(n = 100, directed = FALSE)
+#' dat <- list("nw" = list())
+#' dat <- set_network(dat, nw, 1)
+#'
+set_network <- function(dat, nw, index) {
+
+  ## Warnings and checks
+
+  if (is.list(dat) == FALSE) {
+    stop("Supplied dat object is not of class list",
+         call. = FALSE)
+  }
+
+  if (is.network(nw) == FALSE) {
+    stop("nw is not of class network",
+         call. = FALSE)
+  }
+
+  if (is.numeric(index) == FALSE) {
+    stop("index not a valid position",
+         call. = FALSE)
+  }
+
+  dat[["nw"]][[index]] <- nw
+
+  ## Output -----------------------
+
+  return(dat)
+
+}
 
 #' @title Extract Transmissions Matrix from Network Epidemic Model
 #'
