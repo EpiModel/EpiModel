0f99555c6f415cecb437ccfae675e2fc2a32351f
Shawnee M. Anderson <shawnee.m.anderson@gmail.com>
2020-06-09 09:19:32 -0400

    EpiModel 2.0 Message directing users Epi2 document of changes.

---
+++ R/onload.R
@@ -0,0 +1,6 @@
+.onLoad <- function(libname, pkgname) {
+  message("NOTE: With the release of EpiModel 2.0, significant changes have bee made ",
+          "to the EpiModel workflow, requiring update to user code written using ",
+          "EpiModel version 1.x. Please see https://epimodel.org/tut.html for assistance ",
+          "in migrating your code.")
+}
