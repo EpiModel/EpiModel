dd4969c0c9a5586d0be8da7bc7ef570c01f0e13f
Samuel Jenness <smjenness@gmail.com>
2020-07-27 10:31:52 -0400

    Update onAttach message

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    paste(c(strwrap(
+      paste("EpiModel NOTE: EpiModel 2.0 implements significant changes to the
+            EpiModel application programming interface (API), which may require
+            updates to model code written under EpiModel 1.x. See https://epimodel.org/
+            for migration guidance.",sep = "")), ""), collapse = "\n"))
+}
