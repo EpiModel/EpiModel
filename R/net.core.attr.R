a361d1d2fa7063d3717f49bb64ea7e877e5aa283
Adrien Le Guillou <adrien.leguillou@gmail.com>
2020-12-15 12:20:41 +0100

    Working `append_core_attr` function and refactor
    - `append_core_attr()` creates the required attributes for EpiModel
      ("active" and "uid")
    - `append_core_attr()` replaces the setting of the "active" attribute in
      the init module and arrival functions
    - refactor `append_attr()` function to work when `item` is not present
      yet. It now uses `set_attr` and `get_attr` for a better integration.
    - replacement of `set_attr` by `append_attr` in the "net.mod.init.R" for
      network initiation. It makes the initial creation and the arrival more
      consistent.
    - replacement of `sum(rbinom(nOld, 1, a.rate))` by `rbinom(1, nOld, a.rate)` as
      it is the same thing
    - update "test-merge.R" so it counts 6 attributes and not 5 (because of
      the addition of "uid")

---
+++ R/net.core.attr.R
@@ -0,0 +1,41 @@
+#' @keywords internal
+append_core_attr <- function(dat, n.new) {
+  dat <- append_attr(dat, "active", 1, n.new)
+  dat <- update_uids(dat, n.new)
+
+	return(dat)
+}
+
+#' @keywords internal
+update_uids <- function(dat, n.new) {
+  last_uid <- if (is.null(dat[["_last_uid"]])) 0 else dat[["_last_uid"]]
+  next_uids <- seq_len(n.new) + last_uid
+  dat[["_last_uid"]] <- last_uid + n.new
+  dat <- append_attr(dat, "uid", next_uids, n.new)
+
+  return(dat)
+}
+
+#' @keywords internal
+check_attr_lengths <- function(dat) {
+	attr_lengths <- vapply(dat[["attr"]], length, numeric(1))
+	expected_length <- attr_lengths["active"]
+	wrong_lengths <- which(attr_lengths != expected_length)
+
+	if (length(wrong_lengths > 0)) {
+		msg <- c(
+			"Some attribute are not of the correct length \n",
+			"Expected length: ", expected_length, "\n",
+			"Wrong length attributes: \n"
+		)
+
+		for (i in seq_along(wrong_lengths)) {
+			msg <- c(msg, "`", names(wrong_lengths)[i], "`: ", wrong_lengths[i], "\n")
+		}
+
+		stop(msg)
+	}
+
+  invisible(return(TRUE))
+}
+
