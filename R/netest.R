ef4fb5cee986073236f195cb55e8066c17a7220f
chad-klumb <cklumb@gmail.com>
2021-01-30 14:54:17 -0800

    update for changes in tergm object structure

--- R/netest.R
+++ R/netest.R
@@ -165,15 +165,15 @@ netest <- function(nw, formation, target.stats, coef.diss, constraints,
                   control = set.control.stergm,
                   verbose = verbose)
 
-    coef.form <- fit$fit # there is no longer a separate formation fit
-    which_form <- grep("^Form", names(fit$fit$coef))
+    coef.form <- fit # there is no longer a separate formation fit
+    which_form <- grep("^Form", names(fit$coef))
     
     out <- list()
     out$fit <- fit
     out$formation <- formation
     out$target.stats <- target.stats
     out$target.stats.names <-
-      names(fit$fit$coef[which_form])[!fit$fit$offset[which_form]]
+      names(fit$coef[which_form])[!fit$offset[which_form]]
     out$coef.form <- coef.form$coef[which_form]
     out$dissolution <- dissolution
     out$coef.diss <- coef.diss
