3dc34efd0c8b91c31d69beca997e071227718830
Adrien Le Guillou <adrien.leguillou@gmail.com>
2021-05-25 10:36:12 +0200

    working prototype

--- R/net.mod.trackers.R
+++ R/net.mod.trackers.R
@@ -1,1 +1,115 @@
-# Placeholder
+#' @title Trackers: netsim Module
+#'
+#' @description This function apply the user provided epi trackers
+#'
+#' @param dat Master list object containing a \code{networkDynamic} object and
+#'        other initialization information passed from \code{\link{netsim}}.
+#' @param at Current time step.
+#'
+#' @return the updated dat Master list object.
+#'
+#' @section Optional Module:
+#' This module is not included by default
+#'
+#' @section The \code{epi_trackers} list:
+#' \code{epi_trackers} is a list of NAMED functions stored in the \code{param}
+#' list of the \code{dat} master list object.
+#'
+#' @section Tracker Functions:
+#' This module will apply the tracker functions present in the parameter list
+#' \code{epi_trackers}. Each tracker must be a function with EXACTLY two
+#' arguments: the \code{dat} Master list object and \code{at} the current time
+#' step. They must return a VALUE of length one (numeric, logical or character).
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Create some trackers
+#' epi_prop_infected <- function(dat, at) {
+#'   needed_attributes <- c("status", "active")
+#'   with(get_attr_list(dat, needed_attributes), {
+#'     pop <- active == 1
+#'     cond <- status == "i"
+#'
+#'     out <- sum(cond & pop, na.rm = TRUE) / sum(pop, na.rm = TRUE)
+#'
+#'     return(out)
+#'   })
+#' }
+#'
+#' epi_s_num <- function(dat, at) {
+#'   needed_attributes <- c("status")
+#'   with(get_attr_list(dat, needed_attributes), {
+#'     out <- sum(status == "s", na.rm = TRUE)
+#'
+#'     return(out)
+#'   })
+#' }
+#'
+#' # Create the `epi_trackers` list
+#' epi_trackers <- list(
+#'   prop_infected = epi_prop_infected,
+#'   s_num         = epi_s_num
+#' )
+#'
+#'  # Do not forget to add it to `param`
+#'  param <- param.net(
+#'    inf.prob = 0.3,
+#'    act.rate = 0.5,
+#'    epi_trackers = epi_trackers
+#'  )
+#'
+#' # Enable the module in `control`
+#'  control <- control.net(
+#'    type = NULL, # must be NULL as we use a custom module
+#'    nsims = 2,
+#'    nsteps = 5,
+#'    verbose = FALSE,
+#'    trackers.FUN = trackers.net
+#'  )
+#'
+#'
+#' nw <- network_initialize(n = 50)
+#' nw <- set_vertex_attribute(nw, "race", rbinom(50, 1, 0.5))
+#' est <- netest(
+#'   nw,
+#'   formation = ~edges,
+#'   target.stats = 25,
+#'   coef.diss = dissolution_coefs(~offset(edges), 10, 0),
+#'   verbose = FALSE
+#' )
+#'
+#' init <- init.net(i.num = 10)
+#' mod <- netsim(est, param, init, control)
+#'
+#' df <- as.data.frame(mod)
+#'
+#' df
+#'
+#' }
+#'
+#' @seealso \code{\link{netsim}}
+#'
+#' @export
+#' @keywords netMod internal
+#'
+trackers.net <- function(dat, at) {
+  epi_trackers <- get_param(dat, "epi_trackers", override.null.error = TRUE)
+
+  if (is.null(epi_trackers)) {
+    return(dat)
+  }
+
+  tryCatch(
+    expr = {
+      for (nm in names(epi_trackers)) {
+        dat <- set_epi(dat, nm, at, epi_trackers[[nm]](dat, at))
+      }
+    },
+    message = function(e) message("\nIn tracker '", nm, "':\n", e),
+    warning = function(e) warning("\nIn tracker '", nm, "':\n", e),
+    error = function(e) stop("\nIn tracker '", nm, "':\n", e)
+  )
+
+  return(dat)
+}
