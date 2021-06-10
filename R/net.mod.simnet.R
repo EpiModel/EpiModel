d6e7a57b9df39e527ea12beec1888e8e4a530bc1
Samuel Jenness <smjenness@gmail.com>
2021-06-10 16:24:56 -0400

    Revise resim_nets to allow for static ERGM sims

--- R/net.mod.simnet.R
+++ R/net.mod.simnet.R
@@ -120,17 +120,25 @@ sim_nets_t1 <- function(x, dat, nsteps) {
 #'
 resim_nets <- function(dat, at) {
 
-  # Variables
+  # Control settings for resimulation
   tergmLite <- get_control(dat, "tergmLite")
+  isTERGM <- get_control(dat, "isTERGM")
   save.nwstats <- get_control(dat, "save.nwstats")
   resimulate.network <- get_control(dat, "resimulate.network")
+  nwstats.formula <- get_control(dat, "nwstats.formula")
+  set.control.stergm <- get_control(dat, "set.control.stergm")
+  # mcmc.control.ergm <- get_control(dat, "mcmc.control.ergm")
+  tergmLite.track.duration <- get_control(dat, "tergmLite.track.duration")
 
   # Edges Correction
   dat <- edges_correct(dat, at)
 
+  # active attribute (all models)
   active <- get_attr(dat, "active")
   idsActive <- which(active == 1)
   anyActive <- ifelse(length(idsActive) > 0, TRUE, FALSE)
+
+  # group attribute (built-in models)
   if (dat$param$groups == 2) {
     group <- get_attr(dat, "group")
     groupids.1 <- which(group == 1)
@@ -143,77 +151,82 @@ resim_nets <- function(dat, at) {
   # Pull network model parameters
   nwparam <- get_nwparam(dat)
 
-  # Full tergm/network Method
-  if (tergmLite == FALSE) {
+  # Network resimulation
+  if (anyActive == TRUE & resimulate.network == TRUE) {
 
-    # Set up nwstats df
-    if (save.nwstats == TRUE) {
-      if (at == 2) {
-        nwstats <- attributes(dat$nw[[1]])$stats
-        dat$stats$nwstats[[1]] <- as.data.frame(nwstats)
+    # Full tergm/network Method
+    if (tergmLite == FALSE) {
+
+      # TERGM simulation
+      if (isTERGM == TRUE) {
+        suppressWarnings(
+          dat$nw[[1]] <- simulate(dat$nw[[1]],
+                                  formation = nwparam$formation,
+                                  dissolution = nwparam$coef.diss$dissolution,
+                                  coef.form = nwparam$coef.form,
+                                  coef.diss = nwparam$coef.diss$coef.adj,
+                                  constraints = nwparam$constraints,
+                                  time.start = at,
+                                  time.slices = 1,
+                                  time.offset = 0,
+                                  monitor = nwstats.formula,
+                                  control = set.control.stergm))
+      } else {
+        dat$nw[[1]] <- simulate(object = nwparam$formation,
+                                basis = dat$nw[[1]],
+                                coef = nwparam$coef.form,
+                                # control = mcmc.control.ergm,
+                                dynamic = FALSE,
+                                monitor = nwstats.formula,
+                                nsim = 1)
       }
-    }
 
-    # Network simulation
-    if (anyActive > 0 & resimulate.network == TRUE) {
-      suppressWarnings(
-        dat$nw[[1]] <- simulate(dat$nw[[1]],
-                           formation = nwparam$formation,
-                           dissolution = nwparam$coef.diss$dissolution,
-                           coef.form = nwparam$coef.form,
-                           coef.diss = nwparam$coef.diss$coef.adj,
-                           constraints = nwparam$constraints,
-                           time.start = at,
-                           time.slices = 1,
-                           time.offset = 0,
-                           monitor = dat$control$nwstats.formula,
-                           control = dat$control$set.control.stergm))
-
-      # Update nwstats df
+      # Update nwstats data frame
       if (save.nwstats == TRUE) {
+        new.nwstats <- tail(attributes(dat$nw[[1]])$stats, 1)
+        keep.cols <- which(!duplicated(colnames(new.nwstats)))
+        new.nwstats <- new.nwstats[, keep.cols, drop = FALSE]
+        dat$stats$nwstats[[1]] <- rbind(dat$stats$nwstats[[1]], new.nwstats)
+      }
+    }
+
+    # networkLite/tergmLite Method
+    if (tergmLite == TRUE) {
+
+      dat <- tergmLite::updateModelTermInputs(dat)
+
+      if (isTERGM == TRUE) {
+        rv <- tergmLite::simulate_network(state = dat$p[[1]]$state,
+                                          coef = c(nwparam$coef.form,
+                                                   nwparam$coef.diss$coef.adj),
+                                          control = dat$control$mcmc.control[[1]],
+                                          save.changes = TRUE)
+        dat$el[[1]] <- rv$el
+
+        if (tergmLite.track.duration == TRUE) {
+          dat$p[[1]]$state$nw0 %n% "time" <- rv$state$nw0 %n% "time"
+          dat$p[[1]]$state$nw0 %n% "lasttoggle" <- rv$state$nw0 %n% "lasttoggle"
+        }
+      } else {
+        rv <- tergmLite::simulate_ergm(state = dat$p[[1]]$state,
+                                       coef = nwparam$coef.form,
+                                       control = dat$control$mcmc.control[[1]])
+
+        dat$el[[1]] <- rv$el
+      }
+
+      if (save.nwstats == TRUE) {
+        nwL <- tergmLite::networkLite(dat$el[[1]], dat$attr)
+        if (tergmLite.track.duration == TRUE) {
+          nwL %n% "time" <- dat$p[[1]]$state$nw0 %n% "time"
+          nwL %n% "lasttoggle" <- dat$p[[1]]$state$nw0 %n% "lasttoggle"
+        }
         dat$stats$nwstats[[1]] <- rbind(dat$stats$nwstats[[1]],
-                                   tail(attributes(dat$nw[[1]])$stats, 1)[, ])
+                                          summary(dat$control$nwstats.formulas[[1]],
+                                                  basis = nwL,
+                                                  term.options = dat$control$mcmc.control[[1]]$term.options))
       }
     }
-  }
-
-  # networkLite/tergmLite Method
-  if (tergmLite == TRUE & resimulate.network == TRUE) {
-    isTERGM <- ifelse(nwparam$coef.diss$duration > 1, TRUE, FALSE)
-    dat <- tergmLite::updateModelTermInputs(dat)
-    
-    if (isTERGM == TRUE) {
-      rv <- tergmLite::simulate_network(state = dat$p[[1]]$state,
-                                        coef = c(nwparam$coef.form,
-                                                 nwparam$coef.diss$coef.adj),
-                                        control = dat$control$mcmc.control[[1]],
-                                        save.changes = TRUE)
-
-      dat$el[[1]] <- rv$el
-
-      if (dat$control$tergmLite.track.duration) {
-        dat$p[[1]]$state$nw0 %n% "time" <- rv$state$nw0 %n% "time"
-        dat$p[[1]]$state$nw0 %n% "lasttoggle" <- rv$state$nw0 %n% "lasttoggle"
-      }
-    } else {
-      rv <- tergmLite::simulate_ergm(state = dat$p[[1]]$state,
-                                     coef = nwparam$coef.form,
-                                     control = dat$control$mcmc.control[[1]])
-
-      dat$el[[1]] <- rv$el
-    }
-    
-    if (dat$control$save.nwstats == TRUE) {
-      nwL <- networkLite(dat$el[[1]], dat$attr)
-      if (dat$control$tergmLite.track.duration) {
-        nwL %n% "time" <- dat$p[[1]]$state$nw0 %n% "time"
-        nwL %n% "lasttoggle" <- dat$p[[1]]$state$nw0 %n% "lasttoggle"
-      }
-      dat$stats$nwstats[[1]] <- rbind(dat$stats$nwstats[[1]],
-                                        summary(dat$control$nwstats.formulas[[1]], 
-                                                basis = nwL, 
-                                                term.options = dat$control$mcmc.control[[1]]$term.options))
-    }    
   }
 
   return(dat)
