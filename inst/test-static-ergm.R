6d6fead07febaa97e14c00886820bda24d6e5cda
Samuel Jenness <smjenness@gmail.com>
2021-06-10 16:30:48 -0400

    Unit tests (extended ones staying in inst/ for now)

--- inst/test-static-ergm.R
+++ inst/test-static-ergm.R
@@ -1,26 +1,212 @@
 
-num=20
-toy <- network_initialize(n=num, directed=F)
+## Extended tests for reimplementation of network simulations in EpiModel v2.1.0
 
-ft <- netest(toy,  formation = ~edges ,
-             target.stats = c(100),
-             coef.diss = dissolution_coefs(dissolution = ~offset(edges), duration = 1), edapprox = T)
-
-nws <- list()
-for (i in 1:10) {
-  nws[[i]] <- simulate(ft$fit)
-}
-
-nwd <- networkDynamic(network.list = nws[1], onsets = 1, termini = 1, end = 10)
-as.data.frame(nwd)
-
-
-
+num = 20
+toy <- network_initialize(num)
 param <- param.net(inf.prob = 0.2, act.rate = 1, rec.rate = 1/3)
 init <- init.net(i.num = 10, r.num = 0)
-control <- control.net(type = "SIR", nsteps = 20, nsims = 1, ncores = 1,  tergmLite = F,  resimulate.network = T)
 
+# TERGM resimulate.network = TRUE tergmLite = FALSE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 10),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = TRUE, verbose = FALSE)
 simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
 
-dat <- as.data.frame(simz$network$sim1)
-table(dat$duration)  # now all = 1
+# TERGM resimulate.network = FALSE tergmLite = FALSE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 10),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = FALSE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# ERGM resimulate.network = TRUE tergmLite = FALSE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 1),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# ERGM resimulate.network = FALSE tergmLite = FALSE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 1),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = FALSE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = TRUE tergmLite = TRUE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 10),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = TRUE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+# nwd <- as.data.frame(net)
+# head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = FALSE tergmLite = TRUE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 10),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = TRUE,  resimulate.network = FALSE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# ERGM resimulate.network = TRUE tergmLite = TRUE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 1),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = TRUE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# ERGM resimulate.network = FALSE tergmLite = TRUE (resets to resim = TRUE)
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 1),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = TRUE,  resimulate.network = FALSE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+
+# TERGM resimulate.network = TRUE tergmLite = FALSE full STERGM = TRUE
+ft <- netest(toy,  formation = ~edges ,
+             target.stats = 100,
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges),
+                                           duration = 10),
+             edapprox = FALSE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = FALSE tergmLite = FALSE full STERGM = TRUE
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = FALSE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = TRUE tergmLite = TRUE full STERGM = TRUE
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = TRUE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = FALSE tergmLite = TRUE full STERGM = TRUE  (resets to resim = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = TRUE,  resimulate.network = FALSE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = FALSE tergmLite = FALSE full STERGM = FALSE multi dur = TRUE
+num = 20
+toy <- network_initialize(num)
+toy <- set_vertex_attribute(toy, "age.grp", rbinom(num, 1, 0.5))
+get_vertex_attribute(toy, "age.grp")
+
+ft <- netest(toy,  formation = ~edges + nodematch("age.grp"),
+             target.stats = c(100, 50),
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges) + offset(nodematch("age.grp")),
+                                           duration = c(10, 20)),
+             edapprox = TRUE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = FALSE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = TRUE tergmLite = FALSE full STERGM = FALSE multi dur = TRUE
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = TRUE tergmLite = TRUE full STERGM = FALSE multi dur = TRUE
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = TRUE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
+
+# TERGM resimulate.network = TRUE tergmLite = FALSE full STERGM = TRUE multi dur = TRUE
+ft <- netest(toy,  formation = ~edges + nodematch("age.grp"),
+             target.stats = c(100, 50),
+             coef.diss = dissolution_coefs(dissolution = ~offset(edges) + offset(nodematch("age.grp")),
+                                           duration = c(10, 20)),
+             edapprox = FALSE)
+control <- control.net(type = "SIR", nsteps = 10, nsims = 2, ncores = 1,
+                       tergmLite = FALSE,  resimulate.network = TRUE, verbose = FALSE)
+simz <- netsim(ft, param, init, control)
+net <- get_network(simz, sim = 2)
+nwd <- as.data.frame(net)
+head(nwd, 10)
+get_nwstats(simz)
--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -41,7 +41,7 @@ test_that("get_network yields warning for incorrect sim", {
 
 test_that("get_network error flags", {
   expect_error(get_network(list(a = 1), 1), "must be of class netsim")
-  expect_error(get_network(mod, 4), "Specify sim between 1 and 3")
+  expect_error(get_network(mod, 4), "Specify a single sim between 1 and 3")
   expect_error(get_network(mod, 1, collapse = TRUE), "Specify collapse time")
   expect_error(get_network(mod, 1, collapse = TRUE, at = 200),
                "Specify collapse time")
