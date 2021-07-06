07103c89e97d74035b1e5515b1c69191af4896f7
Adrien Le Guillou <adrien.leguillou@gmail.com>
2021-07-06 16:10:17 +0200

    fix issue 553

--- R/print.r
+++ R/print.r
@@ -316,6 +316,7 @@ print.param.net <- function(x, ...) {
     pToPrint <- pToPrint[! names(x)[pToPrint] %in% names(rng_values)]
   } else if (randoms[1] %in% names(x)) {
     rng_defs <- names(x[[randoms[1]]])
+    pToPrint <- pToPrint[! names(x)[pToPrint] %in% rng_defs]
   }
 
   cat("Fixed Parameters")
