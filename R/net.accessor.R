75760fad1053930bd5a82b1cb9219a41fb73a0e4
Samuel Jenness <smjenness@gmail.com>
2021-10-29 11:36:17 -0400

    Fix error message grammar

--- R/net.accessor.R
+++ R/net.accessor.R
@@ -175,7 +175,7 @@ set_attr <- function(dat, item, value, posit_ids = NULL,
       length(value) != length(dat[["attr"]][["active"]])) {
       stop(
         "When trying to edit the ", `item`, " nodal attribute: ",
-        "The size of the `value` vector is not equal to the number of node in",
+        "The size of the `value` vector is not equal to the number of nodes in",
         " the network. \n",
         "Expected: ", length(dat[["attr"]][["active"]]), "\n",
         "Given: ", length(value)
@@ -202,7 +202,7 @@ set_attr <- function(dat, item, value, posit_ids = NULL,
       length(value) != length(dat[["attr"]][["active"]][posit_ids])) {
       stop(
         "When trying to edit the `", item, "` nodal attribute: ",
-        "The size of the `value` vector is not equal to the number of node ",
+        "The size of the `value` vector is not equal to the number of nodes ",
         "selected by the `posit_ids` vector nor of length 1. \n",
         "Expected: ", length(dat[["attr"]][["active"]][posit_ids]), " or 1 \n",
         "Given: ", length(value)
