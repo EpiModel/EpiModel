25eae6d594990faa81f902a21882f05170bf8289
Adrien Le Guillou <git@aleguillou.org>
2022-01-19 12:53:00 +0100

    from list to df

--- R/param2df.R
+++ R/param2df.R
@@ -118,6 +118,12 @@ make_scenarios_updaters <- function(scenarios.df, at) {
   names(updaters.list) <- names(scenarios.list)
   return(updaters.list)
 }
+
+make_scenarios_df <- function(scenarios.list) {
+  lapply(scenarios.list, flatten_params) %>%
+    bind_rows() %>%
+    mutate(.scenario.id = names(scenarios.list))
+}
 # update params to create scenarios
 #
 # params_orig <- function() {
