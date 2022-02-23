c19362ca82e42a2b3de3802825c91a89b77b5428
Adrien Le Guillou <git@aleguillou.org>
2022-02-23 16:36:13 +0100

    Corrections and make_param_from_df
    - fix updater message "where" -> "were"
    - fix conflict in NEWS (2.2.2)
    - make `use_scenario` error if a parameter in a scenario is not in one
      defined in the `param` object (+ corresponding tests)
    - implementation, documentation and tests of `make_param_from_df`
    
    ToDo:
      1. find a name better than `make_param_from_df` @smjenness any
         suggestion?
      2. add this function to the "model-parameters" vignette

---
+++ tests/testthat/test-parameters-df.R
@@ -0,0 +1,46 @@
+context("Load parameters from data.frame")
+
+test_that("Load parameters from data.frame", {
+  params.df <- dplyr::tribble(
+    ~param, ~value, ~type, ~detail,
+    "p1", "10", "numeric", "foo",
+    "p2", "TRUE", "logical", "bar",
+    "p3_1", "1", "numeric", "baz",
+    "p3_2", "3", "numeric", "foobar",
+    "p4", "tsa", "character", "foobaz"
+  )
+
+  expect_silent(param <- make_param_from_df(params.df))
+  expect_s3_class(param, "param.net")
+  expect_type(param, "list")
+
+  # wrong column name
+  params.df <- dplyr::tribble(~name, ~value, ~type, "p1", "10", "numeric")
+  expect_error(param <- make_param_from_df(params.df))
+  params.df <- dplyr::tribble(~param, ~val, ~type, "p1", "10", "numeric")
+  expect_error(param <- make_param_from_df(params.df))
+  params.df <- dplyr::tribble(~param, ~value, ~class, "p1", "10", "numeric")
+  expect_error(param <- make_param_from_df(params.df))
+
+  # wrong "type" value
+  params.df <- dplyr::tribble(
+    ~param, ~value, ~type, ~detail,
+    "p1", "10", "numeric", "foo",
+    "p2", "TRUE", "logical", "bar",
+    "p3_1", "1", "factor", "baz",
+    "p3_2", "3", "numeric", "foobar",
+    "p4", "tsa", "character", "foobaz"
+  )
+  expect_error(param <- make_param_from_df(params.df))
+
+  # wrong "param" format
+  params.df <- dplyr::tribble(
+    ~param, ~value, ~type, ~detail,
+    ".p1", "10", "numeric", "foo",
+    "p2", "TRUE", "logical", "bar",
+    "p_3_1", "1", "numeric", "baz",
+    "p3_2", "3", "numeric", "foobar",
+    "p4", "tsa", "character", "foobaz"
+  )
+  expect_error(param <- make_param_from_df(params.df))
+})
