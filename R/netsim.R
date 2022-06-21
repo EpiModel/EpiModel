60a02e6a1eb536d8554687979c5feceb9f06e98f
Adrien Le Guillou <git@aleguillou.org>
2022-06-21 17:11:03 +0200

    test without sim.lines = true in dx12 test

--- R/netsim.R
+++ R/netsim.R
@@ -224,3 +224,59 @@ netsim_run <- function(dat, s = 1) {
   dat <- netsim_run_nsteps(dat, steps_to_run, s)
   return(dat)
 }
+
+netsim_loop <- function(x, param, init, control, s) {
+  ## Instantiate random parameters
+  param <- generate_random_params(param, verbose = FALSE)
+
+  dat <- withCallingHandlers(
+    expr = {
+      ## Initialization Module
+      if (!is.null(control[["initialize.FUN"]])) {
+        current_mod <- "initialize.FUN"
+        at <- paste0("`Initialization Step` (", control$start, ")")
+        dat <- do.call(control[[current_mod]], list(x, param, init, control, s))
+      }
+
+      ### TIME LOOP
+      if (control$nsteps > 1) {
+        for (at in max(2, control$start):control$nsteps) {
+          current_mod <- "epimodel.internal"
+          dat <- set_current_timestep(dat, at)
+          # Applies updaters, if any
+          dat <- input_updater(dat)
+
+          ## Module order
+          morder <- get_control(dat, "module.order", override.null.error = TRUE)
+          if (is.null(morder)) {
+            bi.mods <- get_control(dat, "bi.mods")
+            user.mods <- get_control(dat, "user.mods")
+            lim.bi.mods <- bi.mods[
+              -which(bi.mods %in% c("initialize.FUN", "verbose.FUN"))]
+            morder <- c(user.mods, lim.bi.mods)
+          }
+
+          ## Evaluate modules
+          for (i in seq_along(morder)) {
+            current_mod <- morder[[i]]
+            mod.FUN <- get_control(dat, current_mod)
+            dat <- do.call(mod.FUN, list(dat, at))
+          }
+
+          ## Verbose module
+          verbose <- !is.null(control[["verbose.FUN"]])
+          if (verbose == TRUE) {
+            current_mod <- "verbose.FUN"
+            do.call(control[[current_mod]], list(dat, type = "progress", s, at))
+          }
+        }
+      }
+
+      dat
+    },
+    message = function(e) message(netsim_cond_msg("MESSAGE", current_mod, at)),
+    warning = function(e) message(netsim_cond_msg("WARNING", current_mod, at)),
+    error = function(e) message(netsim_cond_msg("ERROR", current_mod, at)))
+
+  return(dat)
+}
