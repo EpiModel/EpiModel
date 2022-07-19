da60b71522c882666b7ac762b7e8c2c2a2a6d10a
chad-klumb <cklumb@gmail.com>
2022-07-18 18:58:24 -0700

    clean up and expand make_stats_table tests

--- tests/testthat/test-netdx.R
+++ tests/testthat/test-netdx.R
@@ -456,13 +456,17 @@ test_that("z scores are not large for a reasonably long simulation", {
 
 test_that("make_stats_table behaves as expected", {
   stat_names <- letters[1:5]
-  m1 <- matrix(1:5, ncol = 5, nrow = 10, byrow = TRUE)
-  m2 <- matrix(c(6,9,5,2,7), ncol = 5, nrow = 10, byrow = TRUE)
+  stats_1 <- 1:5
+  stats_2 <- c(6,9,5,2,7)
+  m1 <- matrix(stats_1, ncol = 5, nrow = 10, byrow = TRUE)
+  m2 <- matrix(stats_2, ncol = 5, nrow = 10, byrow = TRUE)
   colnames(m2) <- colnames(m1) <- stat_names
   stats <- list(m1, m2)
   targets <- c(a = 3, f = 6, e = 7, c = 9, x = 10, d = 2000)
+  targs <- c(3, NA, 9, 2000, 7)
   mst <- make_stats_table(stats, targets)
-  expect_identical(rownames(mst), colnames(m1))
-  expect_identical(mst[["Sim Mean"]], c(1 + 6, 2 + 9, 3 + 5, 4 + 2, 5 + 7)/2)
-  expect_identical(mst[["Target"]], c(3, NA, 9, 2000, 7))
+  expect_equal(rownames(mst), colnames(m1))
+  expect_equal(mst[["Sim Mean"]], (stats_1 + stats_2)/2)
+  expect_equal(mst[["Target"]], targs)
+  expect_equal(mst[["Pct Diff"]], 100*((stats_1 + stats_2)/2 - targs)/targs)
 })
