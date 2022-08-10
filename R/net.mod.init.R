1c6d81489133736b46e65714ffb413beecb604d8
chad-klumb <cklumb@gmail.com>
2022-08-10 06:55:18 -0700

    update stats initialization

--- R/net.mod.init.R
+++ R/net.mod.init.R
@@ -48,6 +48,10 @@ initialize.net <- function(x, param, init, control, s) {
       dat$temp$nwterms <- nwterms
       dat$temp$t1.tab <- get_attr_prop(dat, nwterms)
     }
+    
+    if (get_control(dat, "save.nwstats") == TRUE) {
+      dat$stats$nwstats <- rep(list(), length.out = length(dat$nwparam))
+    }
 
     # Handle "formation" monitor and simulate first time step
     for (network in seq_along(dat$nwparam)) {
