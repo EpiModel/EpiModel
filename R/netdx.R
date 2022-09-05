f171463e0267f6e116149907886dd589af269c2e
chad-klumb <cklumb@gmail.com>
2022-09-05 14:00:03 -0700

    return to original dropping of nwd

--- R/netdx.R
+++ R/netdx.R
@@ -456,9 +456,7 @@ tedgelist_to_toggles <- function(tedgelist) {
 #' @keywords internal
 toggles_to_diss_stats <- function(toggles, coef.diss,
                                   nsteps, nw, time.start = 0L) {
-  if (is(nw, "networkDynamic")) {
-    nw <- as.network(nw) # drop nwd
-  }
+  nw <- as.network(nw) # drop nwd
   delete.network.attribute(nw, "time")
   delete.network.attribute(nw, "lasttoggle")
   nw[, ] <- FALSE
