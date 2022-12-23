0bc99cb40aa877f9b9bd3f1f2322c49ce0369378
chad-klumb <cklumb@gmail.com>
2022-12-23 10:57:54 -0800

    extend multinetwork tests to open populations

--- tests/testthat/test-multinets.R
+++ tests/testthat/test-multinets.R
@@ -1,6 +1,6 @@
 context("multiple network models")
 
-test_that("netsim runs with multiple networks, assuming a closed population", {
+test_that("netsim runs with multiple networks, with open or closed population", {
   nw <- network_initialize(n = 50)
   nw <- set_vertex_attribute(nw, "race", rbinom(50, 1, 0.5))
 
@@ -18,7 +18,10 @@ test_that("netsim runs with multiple networks, assuming a closed population", {
 
   est <- list(est10, est1, est20, est1, est20)
 
-  param <- param.net(inf.prob = 0.3, act.rate = 0.1)
+  param_open <- param.net(inf.prob = 0.3, act.rate = 0.1, a.rate = 0.02,
+                          di.rate = 0.02, ds.rate = 0.02)
+  param_closed <- param.net(inf.prob = 0.3, act.rate = 0.1)
+
   init <- init.net(i.num = 10)
 
   nsims <- 3L
@@ -51,124 +54,148 @@ test_that("netsim runs with multiple networks, assuming a closed population", {
 
   for (tergmLite in c(FALSE, TRUE)) {
     for (resimulate.network in unique(c(tergmLite, TRUE))) {
-      if (tergmLite == TRUE) {
-        save.other <- c("nw", "attr", "el", "temp")
-      } else {
-        save.other <- c("nw", "attr", "temp")      
-      }
+      for (open_population in unique(c(FALSE, resimulate.network))) {
+        if (tergmLite == TRUE) {
+          save.other <- c("nw", "attr", "el", "temp")
+        } else {
+          save.other <- c("nw", "attr", "temp")
+        }
+        if (open_population == TRUE) {
+          param <- param_open
+        } else {
+          param <- param_closed
+        }
+        if (resimulate.network == TRUE) {
+          sim_stat_names <- resim_stat_names
+          sim_nwstats_formulas <- resim_nwstats_formulas
+        } else {
+          sim_stat_names <- stat_names
+          sim_nwstats_formulas <- nwstats_formulas
+        }
 
-      if (resimulate.network == TRUE) {
-        sim_stat_names <- resim_stat_names
-        sim_nwstats_formulas <- resim_nwstats_formulas
-      } else {
-        sim_stat_names <- stat_names
-        sim_nwstats_formulas <- nwstats_formulas
-      }
-
-      for (iteration in 1:2) {
-        if (iteration == 1) {
-          nsteps <- 5L
-          control <- control.net(type = "SI", 
-                                 nsims = nsims, 
-                                 nsteps = nsteps,
-                                 tergmLite = tergmLite,
-                                 resimulate.network = resimulate.network,
-                                 tergmLite.track.duration = TRUE,
-                                 dat.updates = function (dat, at, network) {
-                                   if (network > 0L) {
-                                     if (get_control(dat, "tergmLite") == TRUE) {
-                                       dat <- set_attr(dat, paste0("deg.", network),
-                                                       get_degree(dat$el[[network]]))
-                                     } else {
-                                       dat <- set_attr(dat, paste0("deg.", network),
-                                                       get_degree(as.edgelist(network.collapse(dat$nw[[network]], at = at))))
-                                       for (other_net in seq_along(dat$nwparam)) {
-                                         dat$nw[[other_net]] <- set_vertex_attribute(dat$nw[[other_net]], paste0("deg.", network), get_attr(dat, paste0("deg.", network)))
+        for (iteration in 1:2) {
+          if (iteration == 1) {
+            nsteps <- 5L
+            control <- control.net(type = "SI",
+                                   nsims = nsims,
+                                   nsteps = nsteps,
+                                   tergmLite = tergmLite,
+                                   resimulate.network = resimulate.network,
+                                   tergmLite.track.duration = TRUE,
+                                   dat.updates = function (dat, at, network) {
+                                     if (get_control(dat, "resimulate.network") == TRUE && get_control(dat, "start") > 1L) {
+                                       dat$temp <- temp
+                                     }
+                                     if (network > 0L) {
+                                       if (get_control(dat, "tergmLite") == TRUE) {
+                                         dat <- set_attr(dat, paste0("deg.", network),
+                                                         get_degree(dat$el[[network]]))
+                                       } else {
+                                         deg_attr <- get_degree(as.edgelist(network.collapse(dat$nw[[network]],
+                                                                                             at = at,
+                                                                                             retain.all.vertices = TRUE)))
+                                         for (other_net in seq_along(dat$nwparam)) {
+                                           dat$nw[[other_net]] <- set_vertex_attribute(dat$nw[[other_net]],
+                                                                                       paste0("deg.", network),
+                                                                                       deg_attr)
+                                         }
                                        }
                                      }
-                                   }
-                                   dat
-                                 },
-                                 nwstats.formula = sim_nwstats_formulas,
-                                 verbose = TRUE,
-                                 save.network = !tergmLite,
-                                 save.other = save.other)
-          print(control)
-          basis <- est
-        } else {
-          if (tergmLite == TRUE) {
-            next
-          }
-          control$start <- nsteps + 1L
-          nsteps <- 11L
-          control$nsteps <- nsteps
-          basis <- sim          
-        }
-        sim <- netsim(basis, param, init, control)
-
-        print(sim)
-        plot(sim)
-        plot(sim, type = "formation")
-        if (tergmLite == FALSE) {
-          plot(sim, type = "dissolution")
-          plot(sim, type = "duration")
-        }
-
-        for (network in seq_len(nnets)) {
-          print(sim, network = network)
-          plot(sim, network = network)
-          plot(sim, type = "formation", network = network)
-          if (tergmLite == FALSE) {
-            plot(sim, type = "dissolution", network = network)
-            plot(sim, type = "duration", network = network)
-          }
-        }
-
-        for(simno in seq_len(nsims)) {
-          for(network in seq_len(nnets)) {
+                                     dat
+                                   },
+                                   nwstats.formula = sim_nwstats_formulas,
+                                   verbose = TRUE,
+                                   save.network = !tergmLite,
+                                   save.other = save.other)
+            print(control)
+            basis <- est
+          } else {
             if (tergmLite == TRUE) {
-              expect_is(sim$nw[[simno]][[network]], "networkLite")
-              expect_equal(sim$el[[simno]][[network]], as.edgelist(sim$nw[[simno]][[network]]))
-              expect_equal(sim$attr[[simno]][[paste0("deg.", network)]], get_degree(sim$el[[simno]][[network]]))
-              if (network %in% tergm_nets) {
-                expect_equal(sim$nw[[simno]][[network]] %n% "time", nsteps)
-                expect_true(all((sim$nw[[simno]][[network]] %n% "lasttoggle")[,3] >= 0))
-                expect_true(all((sim$nw[[simno]][[network]] %n% "lasttoggle")[,3] <= nsteps))
-              }
-            } else {
-              expect_is(sim$nw[[simno]][[network]], "networkDynamic")
-              if (resimulate.network == TRUE || iteration == 2) {
-                expect_equal(sim$attr[[simno]][[paste0("deg.", network)]],
-                             get_degree(as.edgelist(network.collapse(sim$network[[simno]][[network]], at = nsteps))))
-              }
+              next
             }
+            control$start <- nsteps + 1L
+            nsteps <- 11L
+            control$nsteps <- nsteps
+            basis <- sim
+          }
+          sim <- netsim(basis, param, init, control)
+          temp <- sim$temp$sim1
 
+          print(sim)
+          plot(sim)
+          plot(sim, type = "formation")
+          if (tergmLite == FALSE) {
+            plot(sim, type = "dissolution")
+            plot(sim, type = "duration")
+          }
+
+          for (network in seq_len(nnets)) {
+            print(sim, network = network)
+            plot(sim, network = network)
+            plot(sim, type = "formation", network = network)
+            if (tergmLite == FALSE) {
+              plot(sim, type = "dissolution", network = network)
+              plot(sim, type = "duration", network = network)
+            }
             expect_equal(sim$nwparam[[network]]$coef.form[1],
                          est[[network]]$coef.form[1] +
                            log(network.size(est[[network]]$newnetwork)) -
-                           log(network.size(sim$nw[[simno]][[network]])))
+                           log(sim$epi$sim.num[nsteps, 1]))
+          }
 
-            expect_equal(network.size(est[[network]]$newnetwork), sim$epi$num[[simno]][1])
-            expect_equal(network.size(sim$nw[[simno]][[network]]), sim$epi$num[[simno]][nsteps])
-
-            stats_matrix <- get_nwstats(sim, network = network, mode = "list")[[simno]]
-            expect_equal(NCOL(stats_matrix), length(sim_stat_names[[network]]))
-            expect_equal(NROW(stats_matrix), nsteps)
-            expect_equal(colnames(stats_matrix), sim_stat_names[[network]])
-
-            final_stats <- stats_matrix[nsteps,]
-            names(final_stats) <- colnames(stats_matrix)
-            if (tergmLite == TRUE) {
-              nwL <- networkLite(sim$el[[simno]][[network]], sim$attr[[simno]])
-              if (network %in% tergm_nets) {
-                nwL %n% "time" <- sim$nw[[simno]][[network]] %n% "time"
-                nwL %n% "lasttoggle" <- sim$nw[[simno]][[network]] %n% "lasttoggle"
+          for(simno in seq_len(nsims)) {
+            for(network in seq_len(nnets)) {
+              if (tergmLite == TRUE) {
+                expect_is(sim$nw[[simno]][[network]], "networkLite")
+                if (open_population == FALSE) {
+                  expect_equal(sim$el[[simno]][[network]], as.edgelist(sim$nw[[simno]][[network]]))
+                  expect_equal(sim$attr[[simno]][[paste0("deg.", network)]], get_degree(sim$el[[simno]][[network]]))
+                }
+                if (network %in% tergm_nets) {
+                  expect_equal(sim$nw[[simno]][[network]] %n% "time", nsteps)
+                  expect_true(all((sim$nw[[simno]][[network]] %n% "lasttoggle")[,3] >= 0))
+                  expect_true(all((sim$nw[[simno]][[network]] %n% "lasttoggle")[,3] <= nsteps))
+                }
+              } else {
+                expect_is(sim$nw[[simno]][[network]], "networkDynamic")
+                if (open_population == FALSE) {
+                  if (resimulate.network == TRUE || iteration == 2) {
+                    expect_equal(sim$nw[[simno]][[network]] %v% paste0("deg.", network),
+                                 get_degree(as.edgelist(network.collapse(sim$network[[simno]][[network]], at = nsteps))))
+                  }
+                }
               }
-              expect_equal(final_stats,
-                           summary(get_network_control(sim, network = network, "nwstats.formula"), basis = nwL))
-            } else {
-              expect_equal(final_stats,
-                           summary(get_network_control(sim, network = network, "nwstats.formula"), at = nsteps, basis = sim$nw[[simno]][[network]])[,,drop=TRUE])
+
+              expect_equal(network.size(est[[network]]$newnetwork), sim$epi$num[[simno]][1])
+              if (tergmLite == TRUE) {
+                expect_equal(network.size(sim$nw[[simno]][[network]]),
+                             sim$epi$sim.num[[simno]][nsteps])
+              } else {
+                expect_equal(network.size(network.collapse(sim$nw[[simno]][[network]], at = nsteps)),
+                             sim$epi$num[[simno]][nsteps])
+              }
+
+              stats_matrix <- get_nwstats(sim, network = network, mode = "list")[[simno]]
+              expect_equal(NCOL(stats_matrix), length(sim_stat_names[[network]]))
+              expect_equal(NROW(stats_matrix), nsteps)
+              expect_equal(colnames(stats_matrix), sim_stat_names[[network]])
+
+              if (open_population == FALSE) {
+                final_stats <- stats_matrix[nsteps,]
+                names(final_stats) <- colnames(stats_matrix)
+                if (tergmLite == TRUE) {
+                  nwL <- networkLite(sim$el[[simno]][[network]], sim$attr[[simno]])
+                  if (network %in% tergm_nets) {
+                    nwL %n% "time" <- sim$nw[[simno]][[network]] %n% "time"
+                    nwL %n% "lasttoggle" <- sim$nw[[simno]][[network]] %n% "lasttoggle"
+                  }
+                  expect_equal(final_stats,
+                               summary(get_network_control(sim, network = network, "nwstats.formula"), basis = nwL))
+                } else {
+                  expect_equal(final_stats,
+                               summary(get_network_control(sim, network = network, "nwstats.formula"), at = nsteps, basis = sim$nw[[simno]][[network]])[,,drop=TRUE])
+                }
+              }
             }
           }
         }
@@ -206,10 +233,10 @@ test_that("multilayer specifications", {
                                                       ~edges + concurrent + mean.age,
                                                       "formation"),
                          verbose = FALSE)
-   
+
   basis <- list(est100, est200, est1, est1, est200)
   sim <- netsim(basis, param, init, control)
-  
+
   for (network in seq_len(5L)) {
     stats <- get_nwstats(sim, network = network)
     if (network < 5L) {
