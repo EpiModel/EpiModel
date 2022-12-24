7a2af3c0e48b1971948d65617cb505c68668ed3a
chad-klumb <cklumb@gmail.com>
2022-12-24 13:54:35 -0800

    make EpiModel gallery case more specific

--- R/net.inputs.R
+++ R/net.inputs.R
@@ -1190,7 +1190,7 @@ crosscheck.net <- function(x, param, init, control) {
     nwparam <- list(x)
   } else if (inherits(x, "netsim")) {
     nwparam <- x$nwparam
-  } else if (inherits(x, "network")) {
+  } else if (inherits(x, "networkDynamic")) {
     nwparam <- list(x) # relevant to EpiModel gallery example
   } else { # must be list of netest
     nwparam <- x
