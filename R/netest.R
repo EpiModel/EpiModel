2b3acacf59d2350000c88becabdbda818d00c389
Adrien Le Guillou <git@aleguillou.org>
2024-10-22 16:39:37 +0200

    MCMC.maxchanges = Inf (#920)
    * MCMC.maxchanges = Inf
    
    * update docs

--- R/netest.R
+++ R/netest.R
@@ -146,7 +146,7 @@
 netest <- function(nw, formation, target.stats, coef.diss, constraints,
                    coef.form = NULL, edapprox = TRUE,
                    set.control.ergm = control.ergm(),
-                   set.control.tergm = control.tergm(),
+                   set.control.tergm = control.tergm(MCMC.maxchanges = Inf),
                    set.control.ergm.ego = NULL,
                    verbose = FALSE, nested.edapprox = TRUE, ...) {
 
